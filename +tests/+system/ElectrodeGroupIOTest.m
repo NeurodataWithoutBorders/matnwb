becb9b716629365ee9e78e1326cd1ca107f6a97e
Lawrence <lancewiu@tutanota.com>
2018-07-13 11:17:09 -0400

    Removed parseClassTest.  Formatting

--- +tests/+system/ElectrodeGroupIOTest.m
+++ +tests/+system/ElectrodeGroupIOTest.m
@@ -1,20 +1,20 @@
 classdef ElectrodeGroupIOTest < tests.system.PyNWBIOTest
-  methods    
-    function addContainer(testCase, file) %#ok<INUSL>
-      dev = types.core.Device( ...
-        'source', 'a test source');
-      file.general_devices.set('dev1', dev);
-      eg = types.core.ElectrodeGroup( ...
-        'source', 'a test source', ...
-        'description', 'a test ElectrodeGroup', ...
-        'location', 'a nonexistent place', ...
-        'device', types.untyped.SoftLink('/general/devices/dev1'));
-      file.general_extracellular_ephys.set('elec1', eg);
+    methods
+        function addContainer(testCase, file) %#ok<INUSL>
+            dev = types.core.Device( ...
+                'source', 'a test source');
+            file.general_devices.set('dev1', dev);
+            eg = types.core.ElectrodeGroup( ...
+                'source', 'a test source', ...
+                'description', 'a test ElectrodeGroup', ...
+                'location', 'a nonexistent place', ...
+                'device', types.untyped.SoftLink('/general/devices/dev1'));
+            file.general_extracellular_ephys.set('elec1', eg);
+        end
+        
+        function c = getContainer(testCase, file) %#ok<INUSL>
+            c = file.general_extracellular_ephys.get('elec1');
+        end
     end
-    
-    function c = getContainer(testCase, file) %#ok<INUSL>
-      c = file.general_extracellular_ephys.get('elec1');
-    end
-  end
 end
 
