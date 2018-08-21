35778050bf3acda06e9b9f01847acbb3e2d50f8f
Lawrence <lancewiu@tutanota.com>
2018-08-21 13:29:04 -0400

    fixed elision on non-groups when reading

--- +io/parseGroup.m
+++ +io/parseGroup.m
@@ -5,29 +5,32 @@ function parsed = parseGroup(filename, info)
 links = containers.Map;
 refs = containers.Map;
 [~, root, ~] = io.pathParts(info.Name);
-[props, typename] = io.parseAttributes(info.Attributes);
+[attrprops, typename] = io.parseAttributes(info.Attributes);
 
 %parse datasets
+dsprops = containers.Map;
 for i=1:length(info.Datasets)
     ds_info = info.Datasets(i);
     fp = [info.Name '/' ds_info.Name];
     ds = io.parseDataset(filename, ds_info, fp);
     if isa(ds, 'containers.Map')
-        props = [props; ds];
+        dsprops = [dsprops; ds];
     else
-        props(ds_info.Name) = ds;
+        dsprops(ds_info.Name) = ds;
     end
 end
 
 %parse subgroups
+gprops = containers.Map;
 for i=1:length(info.Groups)
     g_info = info.Groups(i);
     [~, gname, ~] = io.pathParts(g_info.Name);
     subg = io.parseGroup(filename, g_info);
-    props(gname) = subg;
+    gprops(gname) = subg;
 end
 
 %create link stub
+lprops = containers.Map;
 for i=1:length(info.Links)
     l = info.Links(i);
     fpname = [info.Name '/' l.Name];
@@ -37,11 +40,11 @@ for i=1:length(info.Links)
         otherwise %todo assuming external link here
             lnk = types.untyped.ExternalLink(l.Value{:});
     end
-    props(l.Name) = lnk;
+    lprops(l.Name) = lnk;
 end
 
 if isempty(typename)
-    parsed = types.untyped.Set(props);
+    parsed = types.untyped.Set([attrprops; dsprops; gprops; lprops]);
     
     if isempty(parsed)
         %special case where a directory is simply empty.  Return itself but
@@ -49,17 +52,17 @@ if isempty(typename)
         parsed(root) = [];
     end
 else
-    %elide properties which require elision
-    propnames = keys(props);
+    %elide group properties
+    propnames = keys(gprops);
     typeprops = setdiff(properties(typename), propnames);
     elided_typeprops = typeprops(startsWith(typeprops, propnames));
     for i=1:length(elided_typeprops)
         etp = elided_typeprops{i};
-        props(etp) = elide(etp, props);
+        gprops(etp) = elide(etp, gprops);
     end
     
     %construct as kwargs and instantiate object
-    kwargs = io.map2kwargs(props);
+    kwargs = io.map2kwargs([attrprops; dsprops; gprops; lprops]);
     if isempty(root)
         %we are root
         parsed = nwbfile(kwargs{:});
