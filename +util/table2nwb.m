6a00ff0df958b24daac16d3295fe9d38b788a2b7
bendichter <ben.dichter@gmail.com>
2018-09-22 16:25:15 -0400

    add table2nwb and createUnitTimes to util

---
+++ +util/table2nwb.m
@@ -0,0 +1,24 @@
+function nwbtable = table2nwb(T)
+
+%EXAMPLE
+%   T = table([.1, 1.5, 2.5]',[1., 2., 3.]',[0,1,0]',...
+%       'VariableNames',{'start','stop','condition'});
+%   T.Properties.Description = 'my description';
+%   T.Properties.UserData = containers.Map('source','my source');
+%nwbfile.trials = table2nwb(T)
+
+if ismember('id', T.Properties.VariableNames)
+    id = T.id;
+else
+    id = 0:height(T)-1;
+end
+nwbtable = types.core.DynamicTable( ...
+    'source',T.Properties.UserData('source'),...
+    'colnames', T.Properties.VariableNames,...
+    'description', T.Properties.Description, ...
+    'id', types.core.ElementIdentifiers('data', id));
+
+for col = T
+    nwbtable.tablecolumn.set(col.Properties.VariableNames{1}, ...
+        types.core.TableColumn('data', col.Variables'));
+end
