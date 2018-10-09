9d49aefba5dc16662c5a01ac5ddce93aeb4b2a37
Lawrence <lancewiu@tutanota.com>
2018-10-09 11:06:04 -0400

    flip expected dimensions for MATLAB validation

--- +file/procdims.m
+++ +file/procdims.m
@@ -13,10 +13,12 @@ if iscellstr(sz)
     sz = strrep(sz, 'null', 'Inf');
     emptySz = cellfun('isempty', sz);
     sz(emptySz) = {'Inf'};
+    sz = sz(end:-1:1); %reverse dimensions
     sz = misc.cellPrettyPrint(sz);
 else
     for i=1:length(sz)
         sz{i} = strrep(sz{i}, 'null', 'Inf');
+        sz{i} = sz{i}(end:-1:1); %reverse dimensions
         sz{i} = misc.cellPrettyPrint(sz{i});
     end
 end
