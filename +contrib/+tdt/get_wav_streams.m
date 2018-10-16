6ece78b2634b28347239f1df26ae9f9de155ca74
bendichter <ben.dichter@gmail.com>
2018-10-16 13:48:02 -0700

    add +contrib which contains conversion helper functions for blackrock, tdt, and mworks

---
+++ +contrib/+tdt/get_wav_streams.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = get_wav_streams(inputArg1,inputArg2)
+%GET_WAV_STREAMS Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
