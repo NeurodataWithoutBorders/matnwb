4f32e50e9f03b91d9216fabd046a51a719d0c0bb
Lawrence <lancewiu@tutanota.com>
2018-10-25 10:32:28 -0400

    minor refactor for references.  Better handling for empty references

--- +io/getRefData.m
+++ +io/getRefData.m
@@ -2,9 +2,10 @@ function ref_data = getRefData(fid, ref)
 % defaults to -1 (H5ML.id) which works for H5R.create when using
 % Object References
 refspace = repmat(H5ML.id, size(ref));
+refpaths = {ref.path};
+validPaths = find(~cellfun('isempty', refpaths));
 if isa(ref, 'types.untyped.RegionView')
-    refpaths = {ref.path};
-    for i=1:length(refpaths)
+    for i=validPaths
         did = H5D.open(fid, refpaths{i});
         %by default, we use block mode.
         refspace(i) = ref(i).get_selection(H5D.get_space(did));
@@ -13,7 +14,7 @@ if isa(ref, 'types.untyped.RegionView')
 end
 typesize = H5T.get_size(ref(1).type);
 ref_data = zeros([typesize size(ref)], 'uint8');
-for i=1:numel(ref)
+for i=validPaths
     ref_data(:, i) = H5R.create(fid, ref(i).path, ref(i).reftype, ...
         refspace(i));
 end
