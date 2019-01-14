42ddc52eb1c06cb276775f5c5b9353e3a22dac81
Lawrence <lancewiu@tutanota.com>
2019-01-14 10:13:12 -0500

    fix broken tutorial link.  Fix strings not being variable length

--- +io/getBaseType.m
+++ +io/getBaseType.m
@@ -7,15 +7,7 @@ switch type
     case {'char' 'cell' 'datetime'}
         %modify id to set the proper size
         id = H5T.copy('H5T_C_S1');
-        if iscellstr(data) || isempty(data)
-            %can't make a string of size 0 so empty data must also be variable
-            tsize = 'H5T_VARIABLE';
-        elseif isa(data, 'datetime')
-            tsize = 15; %iso8601 'yyyymmddTHHMMSS'
-        else
-            tsize = size(data, 2);
-        end
-        H5T.set_size(id, tsize);
+        H5T.set_size(id, 'H5T_VARIABLE');
     case 'double'
         id = 'H5T_NATIVE_DOUBLE';
     case 'int64'
--- +io/mapData2H5.m
+++ +io/mapData2H5.m
@@ -4,10 +4,18 @@ function [tid, sid, data] = mapData2H5(fid, type, data)
 %   and properly converted data
 
 tid = io.getBaseType(type, data);
-if ischar(data) && ismatrix(data)
-    sid = H5S.create_simple(1, size(data,1), []);
-    data = data .';
-elseif ~iscell(data) && (isscalar(data) || ischar(data))
+
+if ischar(data)
+    data = mat2cell(data, ones(size(data,1),1), size(data,2));
+    for i=1:length(data)
+        data{i} = char(unicode2native(data{i}));
+    end
+    if isscalar(data)
+        sid = H5S.create('H5S_SCALAR');
+    else
+        sid = H5S.create_simple(1, size(data,1), []);
+    end
+elseif isscalar(data)
     sid = H5S.create('H5S_SCALAR');
 else
     if isvector(data)
@@ -30,6 +38,9 @@ switch type
         %In HDF5, HBOOL is mapped to INT32LE
         data = int32(data);
     case 'datetime'
-        data = datestr(data, 30) .';
+        data = datestr(data, 30);
+        data = mat2cell(data, ones(size(data,1),1), size(data,2));
+        for i=1:length(data)
+            data{i} = char(unicode2native(data{i}));
+        end
 end
-
