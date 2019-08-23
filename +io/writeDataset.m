169f32a39073cd6d73e4c71813032dbe2e522bc9
Lawrence <lancewiu@tutanota.com>
2019-08-23 15:11:30 -0400

    added check when setting extent would cause a crash

--- +io/writeDataset.m
+++ +io/writeDataset.m
@@ -13,9 +13,20 @@ try
 catch ME
     if contains(ME.message, 'name already exists')
         did = H5D.open(fid, fullpath);
-        if ~isempty(dims)
+        create_plist = H5D.get_create_plist(did);
+        edit_sid = H5D.get_space(did);
+        [~, edit_dims, ~] = H5S.get_simple_extent_dims(edit_sid);
+        layout = H5P.get_layout(create_plist);
+        is_chunked = layout == H5ML.get_constant_value('H5D_CHUNKED');
+        is_same_dims = all(edit_dims == dims);
+        if ~is_same_dims && is_chunked
             H5D.set_extent(did, dims);
+        elseif ~is_same_dims
+            warning('Attempted to change size of continuous dataset `%s`.  Skipping.',...
+                fullpath);
         end
+        H5P.close(create_plist);
+        H5S.close(edit_sid);
     else
         rethrow(ME);
     end
