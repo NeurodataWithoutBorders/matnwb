af411e7415248f99608941db1c66c932f6588fe7
Lawrence <lancewiu@tutanota.com>
2019-10-03 18:01:49 -0400

    added new tests for file amending

--- +tests/+system/AmendTest.m
+++ +tests/+system/AmendTest.m
@@ -1,12 +1,20 @@
-classdef RoundTripTest < tests.system.NwbTestInterface
+classdef AmendTest < tests.system.NwbTestInterface
     methods (Test)
-        function testRoundTrip(testCase)
+        function testAmend(testCase)
             filename = ['MatNWB.' testCase.className() '.testRoundTrip.nwb'];
             nwbExport(testCase.file, filename);
+            testCase.appendContainer(testCase.file);
+            nwbExport(testCase.file, filename);
+            
             writeContainer = testCase.getContainer(testCase.file);
             readFile = nwbRead(filename);
             readContainer = testCase.getContainer(readFile);
             testCase.verifyContainerEqual(readContainer, writeContainer);
         end
     end
+    
+    methods (Abstract)
+        appendContainer(testCase, file);
+    end
 end
+
