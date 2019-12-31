75d4c96e16d5f2ebde87cefcd6d31ce9a55e236a
Lawrence <lawrence@vidriotech.com>
2019-12-31 12:12:26 -0500

    DataPipe implementation complete
    Basic tests are working.  Ready for testing.

--- +types/+untyped/DataPipe.m
+++ +types/+untyped/DataPipe.m
@@ -9,6 +9,7 @@ classdef DataPipe < handle
     end
     
     properties (SetAccess = private)
+        isBound; % is associated with a filename and path
         filename;
         path;
         maxSize; % maximum dimension size
@@ -29,14 +30,33 @@ classdef DataPipe < handle
             
             obj.filename = p.Results.filename;
             obj.path = p.Results.path;
-            obj.offset = p.Results.offset;
             obj.axis = p.Results.axis;
+            obj.offset = p.Results.offset;
             obj.chunkSize = p.Results.chunkSize;
             obj.dataType = p.Results.dataType;
         end
     end
     
     methods % get/set
+        function tf = get.isBound(obj)
+            try
+                fid = H5F.open(obj.filename, 'H5F_ACC_RDONLY', 'H5P_DEFAULT');
+                did = H5D.open(fid, obj.path, 'H5P_DEFAULT');
+                
+                tf = true;
+            catch
+                tf = false;
+            end
+            
+            if 1 == exist('fid', 'var')
+                H5F.close(fid);
+            end
+            
+            if 1 == exist('did', 'var')
+                H5D.close(did);
+            end
+        end
+        
         function set.axis(obj, val)
             assert(isscalar(val) && isnumeric(val),...
                 'NWB:Untyped:DataPipe:SetAxis:InvalidType',...
@@ -60,6 +80,7 @@ classdef DataPipe < handle
                 'Offset should be within maxSize bound %d (got %d)',...
                 obj.maxSize(obj.axis),...
                 val);
+            obj.offset = val;
         end
         
         function set.chunkSize(obj, val)
@@ -78,7 +99,7 @@ classdef DataPipe < handle
                 'NWB:Untyped:DataPipe:SetChunkSize:InvalidChunkSize',...
                 '`chunkSize` must be within `maxSize` bounds');
             
-            assert(isempty(obj.filename),...
+            assert(~obj.isBound,...
                 'NWB:Untyped:DataPipe:SetChunkSize:SettingLocked',...
                 ['`chunkSize` cannot be reset if this datapipe is bound to an '...
                 'existing NWB file.']);
@@ -99,7 +120,7 @@ classdef DataPipe < handle
                 '`dataType` must be one of the supported datatypes `%s`',...
                 strjoin(SUPPORTED_DATATYPES, '|'));
             
-            assert(isempty(obj.filename),...
+            assert(~obj.isBound,...
                 'NWB:Untyped:DataPipe:SetDataType:SettingLocked',...
                 ['`dataType` cannot be reset if this datapipe is bound to an '...
                 'existing NWB file.']);
@@ -109,43 +130,69 @@ classdef DataPipe < handle
     end
     
     methods
+        function size = get_size(obj)
+            assert(obj.isBound,...
+                'NWB:Untyped:DataPipe:GetSize:NoAvailableSize',...
+                ['DataPipe must first be bound to a valid hdf5 filename and path to '...
+                'query its current dimensions.']);
+            
+            fid = H5F.open(obj.filename, 'H5F_ACC_RDONLY', 'H5P_DEFAULT');
+            did = H5D.open(fid, obj.path, 'H5P_DEFAULT');
+            sid = H5D.get_space(did);
+            [~, h5_dims, ~] = H5S.get_simple_extent_dims(sid);
+            size = fliplr(h5_dims);
+            H5S.close(sid);
+            H5D.close(did);
+            H5F.close(fid);
+        end
+        
         function append(obj, data)
             assert(isa(data, obj.dataType),...
                 'NWB:Untyped:DataPipe:Append:InvalidType',...
                 'Data must match dataType')
+            assert(obj.isBound,...
+                'NWB:Untyped:DataPipe:ExportRequired',...
+                ['Appending to a dataset requires exporting and re-importing '...
+                    'a valid NWB file.']);
             
-            PLIST = 'H5P_DEFAULT';
-            try
-                fid = H5F.open(obj.filename);
-                did = H5D.open(fid, obj.path);
-            catch ME
-                rethrow(addCause(ME,...
-                    MException(...
-                    'NWB:Untyped:DataPipe:ExportRequired',...
-                    ['Appending to a dataset requires exporting and re-importing '...
-                    'a valid NWB file.'])));
+            if isempty(data)
+                return;
             end
             
+            default_pid = 'H5P_DEFAULT';
+            
+            fid = H5F.open(obj.filename, 'H5F_ACC_RDWR', default_pid);
+            did = H5D.open(fid, obj.path, default_pid);
+            
+            rank = length(obj.maxSize);
+            stride_coords = ones(1, rank);
+            stride_coords(1:length(size(data))) = size(data);
+            new_extents = obj.maxSize;
+            new_extents(obj.axis) = obj.offset + stride_coords(obj.axis) - 1;
+            h5_extents = fliplr(new_extents);
+            H5D.set_extent(did, h5_extents);
+                   
             sid = H5D.get_space(did);
             H5S.select_none(sid);
-            
-            offset_coords = ones(size(obj.max_size));
+
+            offset_coords = ones(1, rank);
             offset_coords(obj.axis) = obj.offset;
             
             h5_start = fliplr(offset_coords) - 1;
-            h5_stride = fliplr(size(data));
-            h5_count = numel(data);
+            h5_stride = [];
+            h5_count = fliplr(stride_coords);
             h5_block = [];
             H5S.select_hyperslab(sid,...
-                'H5S_SELECT_SET',...
+                'H5S_SELECT_OR',...
                 h5_start,...
                 h5_stride,...
                 h5_count,...
                 h5_block);
             
             [mem_tid, mem_sid, data] = io.mapData2H5(fid, data, 'forceArray');
-            
-            H5D.write(did, mem_tid, mem_sid, sid, PLIST, data);
+            H5S.set_extent_simple(mem_sid, rank, h5_count, h5_count);
+
+            H5D.write(did, mem_tid, mem_sid, sid, default_pid, data);
             H5S.close(mem_sid);
             if ~ischar(mem_tid)
                 H5T.close(mem_tid);
@@ -158,33 +205,44 @@ classdef DataPipe < handle
         end
         
         function refs = export(obj, fid, fullpath, refs)
-            if ~isempty(obj.filename) % all data should've been written before export.
+            if obj.isBound
                 return;
             end
             
-            pid = 'H5P_DEFAULT';
+            default_pid = 'H5P_DEFAULT';
+            
+            tid = io.getBaseType(obj.dataType);
             
             rank = length(obj.maxSize);
-            h5_dims = zeros(length(obj.maxSize));
+            h5_dims = zeros(1, rank);
             h5_maxdims = fliplr(obj.maxSize);
+            h5_maxdims(h5_maxdims == Inf) = H5ML.get_constant_value('H5S_UNLIMITED');
             sid = H5S.create_simple(rank, h5_dims, h5_maxdims);
             
-            create_pid = H5P.create('H5P_DATASET_CREATE');
+            lcpl = default_pid;
+
+            dcpl = H5P.create('H5P_DATASET_CREATE');
             if isempty(obj.chunkSize)
-                h5_chunk_dims = fliplr(obj.chunkSize);
+                h5_chunk_dims = h5_maxdims;
             else
-                h5_chunk_dims = h5_max_dims;
+                h5_chunk_dims = fliplr(obj.chunkSize);
             end
-            H5P.set_chunk(create_pid, h5_chunk_dims);
-            tid = io.getBaseType(obj.dataType);
-            did = H5D.create(fid, fullpath, tid, sid, pid, create_pid, pid);
+            H5P.set_chunk(dcpl, h5_chunk_dims);
             
-            H5P.close(create_pid);
+            dapl = default_pid;
+            
+            did = H5D.create(fid, fullpath, tid, sid, lcpl, dcpl, dapl);
+            
+            H5P.close(dcpl);
+            H5S.close(sid);
             if ~ischar(tid)
                 H5T.close(tid);
-            end
-            H5S.close(sid);
+            end            
             H5D.close(did);
+            
+            % bind to this file.
+            obj.filename = H5F.get_name(fid);
+            obj.path = fullpath;
         end
     end
 end
