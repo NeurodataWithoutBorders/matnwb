486985e2ae9c85311db7b6e620c4885f5cc76658
!git for-each-ref --format='%(refname:short)' `git symbolic-ref HEAD` <ben.dichter@gmail.com>
2020-06-01 12:55:09 -0400

    handle in-memory arrays

--- +util/nwbTree.m
+++ +util/nwbTree.m
@@ -28,7 +28,13 @@ if any(strcmp(superclasses(node), 'types.untyped.GroupClass')) || isa(node, 'typ
             elseif isa(new_node, 'char')
                 uitreenode(tree_node, 'Text', [p{1} ': ' new_node]);
             elseif isnumeric(new_node)
-                uitreenode(tree_node, 'Text', [p{1} ': ' num2str(new_node)]);
+                if numel(new_node)  == 1
+                    uitreenode(tree_node, 'Text', [p{1} ': ' num2str(new_node)]);
+                else
+                    data_node = uitreenode(tree_node, 'Text', p{1});
+                    uitreenode(data_node, 'Text', ['shape: [' num2str(size(new_node)) ']'])
+                    uitreenode(data_node, 'Text', ['class: ' class(new_node)])
+                end 
             else
                 uitreenode(tree_node, 'Text', p{1});
             end
@@ -39,6 +45,65 @@ elseif isa(node, 'types.untyped.Set')
         new_tree_node = uitreenode(tree_node, 'Text', key{1});
         traverse_node(node.get(key{1}), new_tree_node)
     end
+end
+
+
+function [ bytes ] = getMemSize( variable, sizelimit, name, indent )
+    if nargin < 2
+        sizelimit = -1;
+    end
+    if nargin < 3
+        name = 'variable';       
+    end
+    if nargin < 4
+        indent = '';
+    end
+    
+    strsize = 30;
+    
+    props = properties(variable); 
+    if size(props, 1) < 1
+        
+        bytes = whos(varname(variable)); 
+        bytes = bytes.bytes;
+        
+        if bytes > sizelimit
+            if bytes < 1024
+                fprintf('%s%s: %i\n', indent, pad(name, strsize - length(indent)), bytes);
+            elseif bytes < 2^20
+                fprintf('%s%s: %i Kb\n', indent, pad(name, strsize - length(indent)), round(bytes / 2^10));
+            elseif bytes < 2^30
+                fprintf('%s%s: %i Mb\n', indent, pad(name, strsize - length(indent)), round(bytes / 2^20));
+            else
+                fprintf('%s%s: %i Gb [!]\n', indent, pad(name, strsize - length(indent)), round(bytes / 2^30));
+            end
+        end
+    else
+        
+        fprintf('\n%s[%s] \n\n', indent, name);
+        bytes = 0;
+        for ii=1:length(props)
+            currentProperty = getfield(variable, char(props(ii)));
+            pp = props(ii);
+            bytes = bytes + getMemSize(currentProperty, sizelimit, pp{1}, [indent, '  ']);
+        end                
+                
+        if length(indent) == 0
+            fprintf('\n');
+            name = 'TOTAL';
+            if bytes < 1024
+                fprintf('%s%s: %i\n', indent, pad(name, strsize - length(indent)), bytes);
+            elseif bytes < 2^20
+                fprintf('%s%s: %i Kb\n', indent, pad(name, strsize - length(indent)), round(bytes / 2^10));
+            elseif bytes < 2^30
+                fprintf('%s%s: %i Mb\n', indent, pad(name, strsize - length(indent)), round(bytes / 2^20));
+            else
+                fprintf('%s%s: %i Gb [!]\n', indent, pad(name, strsize - length(indent)), round(bytes / 2^30));
+            end
+        end
+    
+    end   
+        
 end
 
 
