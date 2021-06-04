6363a46fa775dfdcb879287893a538965945f83e
Lawrence <lawrence@vidriotech.com>
2021-06-04 14:37:55 -0400

    Add External Link deref() tests

--- +tests/+unit/linkTest.m
+++ +tests/+unit/linkTest.m
@@ -45,7 +45,7 @@ testCase.verifyEqual(info.Links.Type, 'external link');
 testCase.verifyEqual(info.Links.Value, {'extern.nwb';'/mypath'});
 end
 
-function testPathResolution(testCase)
+function testSoftResolution(testCase)
 nwb = NwbFile;
 dev = types.core.Device;
 nwb.general_devices.set('testDevice', dev);
@@ -54,4 +54,34 @@ nwb.general_extracellular_ephys.set('testEphys',...
     types.untyped.SoftLink('/general/devices/testDevice')));
 testCase.verifyEqual(dev,...
     nwb.general_extracellular_ephys.get('testEphys').device.deref(nwb));
+end
+
+function testExternalResolution(testCase)
+nwb = NwbFile('identifier', 'EXTERNAL',...
+    'session_description', 'external link test',...
+    'session_start_time', datetime());
+
+expectedData = rand(100,1);
+stubDtr = types.hdmf_common.DynamicTableRegion(...
+    'table', types.untyped.ObjectView('/acquisition/es1'),...
+    'description', 'dtr stub that points to electrical series illegally'); % do not do this at home.
+expected = types.core.ElectricalSeries('data', expectedData,...
+    'data_unit', 'unit',...
+    'electrodes', stubDtr);
+nwb.acquisition.set('es1', expected);
+nwb.export('test1.nwb');
+
+externalLink = types.untyped.ExternalLink('test1.nwb', '/acquisition/es1');
+tests.util.verifyContainerEqual(testCase, externalLink.deref(), expected);
+externalDataLink = types.untyped.ExternalLink('test1.nwb', '/acquisition/es1/data');
+% for datasets, a Datastub is returned.
+testCase.verifyEqual(externalDataLink.deref().load(), expectedData);
+
+nwb.acquisition.clear();
+nwb.acquisition.set('lfp', types.core.LFP('eslink', externalLink));
+nwb.export('test2.nwb');
+
+metaExternalLink = types.untyped.ExternalLink('test2.nwb', '/acquisition/lfp/eslink');
+% for links, deref() should return its own link.
+tests.util.verifyContainerEqual(testCase, metaExternalLink.deref().deref(), expected);
 end
