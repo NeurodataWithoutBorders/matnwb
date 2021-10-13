db9baeaf02302bf7997ecd5f4382f4c283927874
Lawrence <lawrence@vidriotech.com>
2021-10-13 12:13:34 -0400

    add getrow a2a testing

--- +tests/+system/DynamicTableTest.m
+++ +tests/+system/DynamicTableTest.m
@@ -42,11 +42,38 @@ classdef DynamicTableTest < tests.system.RoundTripTest & tests.system.AmendTest
     
     methods (Test)
         function getRowTest(testCase)
+            Table = testCase.file.intervals_trials;
+
+            BaseVectorData = Table.vectordata.get('randomvalues');
+            VectorDataInd = Table.vectordata.get('randomvalues_index');
+            VectorDataIndInd = Table.vectordata.get('randomvalues_index_index');
+
+            endInd = VectorDataIndInd.data(5);
+            startInd = VectorDataIndInd.data(4) + 1;
+
+            Indices = startInd:endInd;
+            dataIndices = cell(length(Indices),1);
+            for iRaggedInd = 1:length(Indices)
+                endInd = VectorDataInd.data(Indices(iRaggedInd));
+                if 1 == Indices(iRaggedInd)
+                    startInd = 1;
+                else
+                    startInd = VectorDataInd.data(Indices(iRaggedInd) - 1) + 1;
+                end
+                dataIndices{iRaggedInd} = BaseVectorData.data((startInd:endInd) .', :);
+            end
+
+            actualData = Table.getRow(5, 'columns', {'randomvalues'});
+            testCase.verifyEqual(dataIndices, actualData.randomvalues{1});
+        end
+
+        function getRowRoundtripTest(testCase)
             filename = ['MatNWB.' testCase.className() '.testGetRow.nwb'];
             nwbExport(testCase.file, filename);
             ActualFile = nwbRead(filename);
             ActualTable = ActualFile.intervals_trials;
             ExpectedTable = testCase.file.intervals_trials;
+
             testCase.verifyEqual(ExpectedTable.getRow(5), ActualTable.getRow(5));
             testCase.verifyEqual(ExpectedTable.getRow([5 6]), ActualTable.getRow([5 6]));
             testCase.verifyEqual(ExpectedTable.getRow([1153, 1217], 'useId', true),...
