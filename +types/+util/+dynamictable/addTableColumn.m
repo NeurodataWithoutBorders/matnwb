263c6cb3d9440615960d31761aa325ebab043a9a
cechava <cesar@Cesars-MacBook-Pro-2.local>
2021-10-22 16:15:56 -0400

    added checks before adding columns; need to clean up code

--- +types/+util/+dynamictable/addTableColumn.m
+++ +types/+util/+dynamictable/addTableColumn.m
@@ -2,11 +2,46 @@ function addTableColumn(DynamicTable, subTable)
 
 newColNames = DynamicTable.validate_colnames(subTable.Properties.VariableNames);
 
+% get current table height
+if ~isempty(DynamicTable.colnames)
+    columnHeight = getColumnsHeight(DynamicTable);
+    tableHeight = columnHeight(1);
+end
+
 for i = 1:length(newColNames)
     new_cn = newColNames{i};
-    new_cv = sub_table.(new_cn);
-    % check that table height matches column height
-
+    new_cv = types.hdmf_common.VectorData( ...
+        'description', 'new column', ...
+        'data', subTable.(new_cn) ...
+        );
+    % check height match before adding column
+    if ~isempty(DynamicTable.colnames)
+        assert(height(new_cv.data) == tableHeight,...
+            'NWB:DynamicTable:AddColumn:MissingRows',...
+            'New column length must match length of existing columns ') 
+    end
     DynamicTable.colnames{end+1} = new_cn;
     DynamicTable.vectordata.set(new_cn, new_cv);   
+end
+end
+function lens = getColumnsHeight(DynamicTable)   
+    columns = keys(DynamicTable.vectordata);
+    c = 1;
+    while c <= length(columns)
+        if ~isempty(types.util.dynamictable.getIndex(DynamicTable,columns{c}))
+            columns(c) = [];
+        else
+            c = c+1;
+        end
+    end
+    lens = zeros(length(columns),1);
+    lens_equal = zeros(length(columns),1);
+    for c = 1:length(columns)
+        lens(c)=length(DynamicTable.vectordata.get(columns{c}).data);
+        lens_equal(c) = lens(1)==lens(c);
+    end
+    assert(all(lens_equal), ...
+        'NWB:DynamicTable', ...
+        'All existing columns must be the same length.' ...
+        );
 end
--- +types/+util/+dynamictable/addVarargColumn.m
+++ +types/+util/+dynamictable/addVarargColumn.m
@@ -1,24 +1,60 @@
 function addVarargColumn(DynamicTable, varargin)
 
 %check proper argument/value inputs and parse
-[newVectorData, ivarargin] = types.util.parseConstrained(DynamicTable,'vectordata', 'types.hdmf_common.VectorData', varargin{:});
+[newVectorData, ~] = types.util.parseConstrained(DynamicTable,'vectordata', 'types.hdmf_common.VectorData', varargin{:});
 
 newColNames = DynamicTable.validate_colnames({varargin{1:2:end}});
-
+% get current table height
+if ~isempty(DynamicTable.colnames)
+    columnHeight = getColumnsHeight(DynamicTable);
+    tableHeight = columnHeight(1);
+end
 
 for i = 1:length(newColNames)
     new_cn = newColNames{i};
     new_cv = newVectorData.get(new_cn);
-%     %check height before adding column
-%     if ~isempty(DynamicTable.colnames) !!! would have to make sure that
-%     chosen column is not a ragged column..
-%         table_height = height(DynamicTable.vectordata.get(DynamicTable.colnames{1}).data);
-%         assert(height(new_cv.data) == table_height,...
-%             'NWB:DynamicTable:AddColumn:MissingRows',...
-%             'New column length must match length of existing columns ') 
-%     end
+    % check height match before adding column
+    if ~isempty(DynamicTable.colnames)
+        assert(height(new_cv.data) == tableHeight,...
+            'NWB:DynamicTable:AddColumn:MissingRows',...
+            'New column length must match length of existing columns ') 
+    end
     if ~isa(new_cv, 'types.hdmf_common.VectorIndex')
         DynamicTable.colnames{end+1} = new_cn;
     end
     DynamicTable.vectordata.set(new_cn, new_cv);   
 end
+end
+
+%scratch
+tmpTable = types.hdmf_common.DynamicTable();
+tmpTable.vectordata = newVectorData;
+ind_name = types.util.dynamictable.getIndex(tmpTable,new_cn);
+ind = newVectorData.get(ind_name);
+
+
+
+function lens = getColumnsHeight(DynamicTable)   
+    columns = keys(DynamicTable.vectordata);
+    c = 1;
+    while c <= length(columns)
+        if ~isempty(types.util.dynamictable.getIndex(DynamicTable,columns{c}))
+            columns(c) = [];
+        else
+            c = c+1;
+        end
+    end
+    lens = zeros(length(columns),1);
+    lens_equal = zeros(length(columns),1);
+    for c = 1:length(columns)
+        lens(c)=length(DynamicTable.vectordata.get(columns{c}).data);
+        lens_equal(c) = lens(1)==lens(c);
+    end
+    assert(all(lens_equal), ...
+        'NWB:DynamicTable', ...
+        'All existing columns must be the same length.' ...
+        );
+end
+
+
+
