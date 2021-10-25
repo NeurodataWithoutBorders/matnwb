59700334ab1d63b4e4dd9ef2d4b4d66e341c649d
cechava <cesar@Cesars-MacBook-Pro-2.local>
2021-10-25 19:40:09 -0400

    updated to account for case when ragged index is not in column names; now passes UnitTimesIOTest

--- +util/checkDynamicTableShape.m
+++ +util/checkDynamicTableShape.m
@@ -5,12 +5,15 @@ function checkDynamicTableShape(DynamicTable)
 % 2) All rows have a corresponding id. If none, exist it creates them.
 
 % keep track of last non-ragged column index; to prevent looping over array twice
+columns = DynamicTable.colnames;
+c = 1;
 lastStraigthCol = 0;
-lens = zeros(length(DynamicTable.colnames),1);
-for c = 1:length(DynamicTable.colnames)
-    cn = DynamicTable.colnames{c};
+lens = zeros(length(columns),1);
+while c <= length(columns)
+    cn = columns{c};
     % ignore columns that have an index (i.e. ragged), length will be unmatched
-    if isempty(types.util.dynamictable.getIndex(DynamicTable, cn))
+    indexName = types.util.dynamictable.getIndex(DynamicTable, cn);
+    if isempty(indexName)
         if isprop(DynamicTable, cn)
             cv = DynamicTable.(cn);
             if ~isempty(cv)
@@ -29,16 +32,22 @@ for c = 1:length(DynamicTable.colnames)
                 );
         end
         lastStraigthCol = c;
+    else
+        if ~any(strcmp(columns,indexName))
+            columns{length(columns)+1} = indexName;
+            
+        end
     end
+    c = c+1;
 end
 
 if ~isempty(lens)
     if isempty(DynamicTable.id)
         DynamicTable.id = types.hdmf_common.ElementIdentifiers( ...
-            'data', int64((1:lens(1))-1)' ...
+            'data', int64((1:lens(lastStraigthCol))-1)' ...
             );
     else
-        assert(lens(1) == length(DynamicTable.id.data), ...
+        assert(lens(lastStraigthCol) == length(DynamicTable.id.data), ...
             'NWB:DynamicTable', ...
             'Must provide same number of ids as length of columns.' ...
         );
