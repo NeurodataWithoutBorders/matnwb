473dfedcff9f2efba7638cdf5f8c6c1de92e7b1c
cechava <cesar@Cesars-MacBook-Pro-2.local>
2021-10-25 18:46:16 -0400

    added support to be able to add ragged array columns

--- +types/+util/+dynamictable/addVarargColumn.m
+++ +types/+util/+dynamictable/addVarargColumn.m
@@ -9,23 +9,34 @@ if ~isempty(DynamicTable.colnames)
     tableHeight = length(DynamicTable.id.data);
 end
 
-
 for i = 1:length(newColNames)
     new_cn = newColNames{i};
     new_cv = newVectorData.get(new_cn);
     % check height match before adding column
     if ~isempty(DynamicTable.colnames)
-        assert(height(new_cv.data) == tableHeight,...
-            'NWB:DynamicTable:AddColumn:MissingRows',...
-            'New column length must match length of existing columns ') 
+        indexName = getIndexInSet(newVectorData,new_cn);
+        if isempty(indexName)
+            assert(height(new_cv.data) == tableHeight,...
+                'NWB:DynamicTable:AddColumn:MissingRows',...
+                'New column length must match length of existing columns ') 
+        else
+            assert(height(newVectorData.get(indexName).data) == tableHeight,...
+                'NWB:DynamicTable:AddColumn:MissingRows',...
+                'New column length must match length of existing columns ') 
+        end
     end
     if ~isa(new_cv, 'types.hdmf_common.VectorIndex')
         DynamicTable.colnames{end+1} = new_cn;
     end
     DynamicTable.vectordata.set(new_cn, new_cv);   
 end
+end
 
-
-
-
+function indexName = getIndexInSet(inputSet, inputName)
+    % wrap input set with an empty dynamic table
+    T = types.hdmf_common.DynamicTable();
+    T.vectordata = inputSet;
+    % use dynamic table function to get index name
+    indexName = types.util.dynamictable.getIndex(T, inputName);
+end
 
