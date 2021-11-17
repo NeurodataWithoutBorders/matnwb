2567a8434070b3b01178dbdae5891abc795283a0
Lawrence <lawrence@mbfbioscience.com>
2021-11-17 16:52:26 -0500

    Add namespace generation from single YAML
    Since everything is stored in a group, you can technically have more than one namespace
    definition in a single YAML file.

--- +spec/generate.m
+++ +spec/generate.m
@@ -1,24 +1,29 @@
-function NamespaceInfo = generate(namespaceText, schemaSource)
+function Namespaces = generate(namespaceText, schemaSource)
 %GENERATE Generates MATLAB classes from namespace mappings.
 % optionally, include schema mapping as second argument OR path of specs
 % schemaSource is either a path to a directory where the source is
 % OR a containers.Map of filenames
 Schema = spec.loadSchemaObject();
 namespace = spec.schema2matlab(Schema.read(namespaceText));
-NamespaceInfo = spec.getNamespaceInfo(namespace);
-NamespaceInfo.namespace = namespace;
-if ischar(schemaSource)
-    schema = containers.Map;
-    for i=1:length(NamespaceInfo.filenames)
-        filenameStub = NamespaceInfo.filenames{i};
-        filename = [filenameStub '.yaml'];
-        fid = fopen(fullfile(schemaSource, filename));
-        schema(filenameStub) = fread(fid, '*char') .';
-        fclose(fid);
+Namespaces = spec.getNamespaceInfo(namespace);
+
+for iInfo = 1:length(Namespaces)
+    Namespaces(iInfo).namespace = namespace;
+    if ischar(schemaSource)
+        schema = containers.Map;
+        Namespace = Namespaces(iInfo);
+        for iFilenames = 1:length(Namespace.filenames)
+            filenameStub = Namespace.filenames{iFilenames};
+            filename = [filenameStub '.yaml'];
+            fid = fopen(fullfile(schemaSource, filename));
+            schema(filenameStub) = fread(fid, '*char') .';
+            fclose(fid);
+        end
+        schema = spec.getSourceInfo(schema);
+    else % map of schemas with their locations
+        schema = spec.getSourceInfo(schemaSource);
     end
-    schema = spec.getSourceInfo(schema);
-else % map of schemas with their locations
-    schema = spec.getSourceInfo(schemaSource);
+    Namespaces(iInfo).schema = schema;
 end
-NamespaceInfo.schema = schema;
+
 end
