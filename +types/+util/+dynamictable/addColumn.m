73f137fc9c494ae82a728a6c58cdd17104e11d72
cechava <cesar@Cesars-MacBook-Pro-2.local>
2021-11-22 15:55:29 -0500

    addColumn utility to DynamicTable method

--- +types/+util/+dynamictable/addColumn.m
+++ +types/+util/+dynamictable/addColumn.m
@@ -1,4 +1,19 @@
 function addColumn(DynamicTable, varargin)
+% ADDCOLUMN Given a dynamic table and a set of keyword arguments for one or
+% more columns, add one or more columns to the dynamic table by providing
+% either keywords or a MATLAB table
+%
+%  ADDCOLUMN(DT,table) append the columns of the MATLAB table to the 
+%  DynamicTable
+%
+%  ADDCOLUMN(DT,col_name1,col_vector1,...,col_namen,col_vectorn)
+%  append specified column names and VectorData to table
+%
+% This function asserts the following:
+% 1) DynamicTable is a valid dynamic table and has the correct
+%    properties.
+% 2) The height of the columns to be appended matches the height of the
+% existing columns
 
 validateattributes(DynamicTable,...
     {'types.core.DynamicTable', 'types.hdmf_common.DynamicTable'},...
