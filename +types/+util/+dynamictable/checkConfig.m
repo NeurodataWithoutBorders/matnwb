893dc0843e28300acf35163984bc0cf49f7c7121
cechava <cesar@Cesars-MacBook-Pro-2.local>
2021-11-24 13:26:27 -0500

    ignoreList option on checkConfig; check table when instantiated; updated tests

--- +types/+util/+dynamictable/checkConfig.m
+++ +types/+util/+dynamictable/checkConfig.m
@@ -1,16 +1,25 @@
-function checkConfig(DynamicTable)
+function checkConfig(DynamicTable, varargin)
 % CHECKCONFIG Given a DynamicTable object, this functions checks for proper
 % DynamicTable configuration
 %
-%   checkConfig(DYNAMICTABLE)
-%   runs without error if the DynamicTable is configured correctly
+%   checkConfig(DYNAMICTABLE) runs without error if the DynamicTable is 
+%   configured correctly
+%
+%   checkConfig(DYNAMICTABLE,IGNORELIST) performs checks on columns not in 
+%   IGNORELIST cell array
+%   
 %
 %  A properly configured DynamicTable should meet the following criteria:
 %  1) The length of all columns in the dynamic table is the same.
 %  2) All rows have a corresponding id. If none exist, this function creates them.
-
+if nargin<2
+    ignoreList = {};
+else
+    ignoreList = varargin{1};
+end
+% do not check specified columns - useful for classes that build on DynamicTable class 
+columns = setdiff(DynamicTable.colnames,ignoreList);
 % keep track of last non-ragged column index; to prevent looping over array twice
-columns = DynamicTable.colnames;
 c = 1;
 lastStraigthCol = 0;
 lens = zeros(length(columns),1);
