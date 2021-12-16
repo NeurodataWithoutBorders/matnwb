045eb9e7cca2e2c299c9e4f1a273568282404c29
cechava <cesar@Cesars-MacBook-Pro-2.local>
2021-12-16 16:45:36 -0500

    passing basic DynamicTable tests with rows-last convention

--- +types/+util/+dynamictable/addTableColumn.m
+++ +types/+util/+dynamictable/addTableColumn.m
@@ -1,31 +1,4 @@
 function addTableColumn(DynamicTable, subTable)
 
-newColNames = DynamicTable.validate_colnames(subTable.Properties.VariableNames);
-
-% get current table height - assume id length reflects table height
-if ~isempty(DynamicTable.colnames)
-    tableHeight = length(DynamicTable.id.data);
-end
-
-for i = 1:length(newColNames)
-    new_cn = newColNames{i};
-     if 8 == exist('types.hdmf_common.VectorData', 'class') 
-         new_cv = types.hdmf_common.VectorData( ...
-        'description', 'new column', ...
-        'data', subTable.(new_cn) ...
-        );
-     else % legacy Element Identifiers 
-        new_cv = types.core.VectorData( ...
-        'description', 'new column', ...
-        'data', subTable.(new_cn) ...
-        );
-     end 
-    % check height match before adding column
-    if ~isempty(DynamicTable.colnames)
-        assert(height(new_cv.data) == tableHeight,...
-            'NWB:DynamicTable:AddColumn:MissingRows',...
-            'New column length must match length of existing columns ') 
-    end
-    DynamicTable.colnames{end+1} = new_cn;
-    DynamicTable.vectordata.set(new_cn, new_cv);   
-end
+error(['Using MATLAB tables as input to the addColumn DynamicTable method has '...
+    'been deprecated. Please, use key-value pairs instead'])
