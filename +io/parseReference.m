614fb57385d974b5ad7781c570a2a0db5c38f15a
cechava <cesar@cesars-mbp-2.attlocal.net>
2021-12-21 10:59:45 -0500

    reversed parseReference transpose

--- +io/parseReference.m
+++ +io/parseReference.m
@@ -3,7 +3,7 @@ szref = size(data);
 %first dimension is always the raw buffer size
 szref = szref(2:end);
 if isscalar(szref)
-    szref = [1 szref];%expect uni-dimensional as column vector
+    szref = [szref 1];
 end
 numref = prod(szref);
 if H5T.equal(tid, 'H5T_STD_REF_OBJ')
