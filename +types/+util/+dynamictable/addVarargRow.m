607e3c222868c03bfadd0d0730e57d42b56c327e
Lawrence <lawrence@mbfbioscience.com>
2022-01-17 17:34:22 -0500

    remove extraneous id append

--- +types/+util/+dynamictable/addVarargRow.m
+++ +types/+util/+dynamictable/addVarargRow.m
@@ -45,7 +45,6 @@ if specifiesId
     newId = p.Results.id;
 elseif isa(DynamicTable.id.data, 'types.untyped.DataPipe')
     newId = DynamicTable.id.data.offset;
-    DynamicTable.id.data.append(DynamicTable.id.data.offset);
 else
     newId = length(DynamicTable.id.data);
 end
