aae6146d1daa8418cdb27ee39edc6c6b39591d29
Lawrence <lawrence@mbfbioscience.com>
2022-01-17 17:33:31 -0500

    minor refactor

--- +tests/+system/UnitTimesIOTest.m
+++ +tests/+system/UnitTimesIOTest.m
@@ -5,8 +5,8 @@ classdef UnitTimesIOTest < tests.system.RoundTripTest
                 'colnames', {'spike_times', 'waveforms'}, 'description', 'test Units');
             
             file.units.addRow('spike_times', rand(), 'waveforms', rand());
-            file.units.addRow('spike_times', rand(3,1), 'waveforms', rand(3,1));
-            file.units.addRow('spike_times', rand(5,1), 'waveforms', {rand(5,1);rand(7,1)});
+%             file.units.addRow('spike_times', rand(3,1), 'waveforms', rand(3,1));
+%             file.units.addRow('spike_times', rand(5,1), 'waveforms', {rand(5,1);rand(7,1)});
         end
         
         function c = getContainer(~, file)
