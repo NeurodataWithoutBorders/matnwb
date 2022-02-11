a7c4b8540b7ee1e19fe075fd2b39e848c19ead82
Lawrence <lawrence@mbfbioscience.com>
2022-02-11 15:06:04 -0500

    refactor generated doc strings

--- +file/fillProps.m
+++ +file/fillProps.m
@@ -1,33 +1,83 @@
-function s = fillProps(props, names, options)
+function s = fillProps(props, names, varargin)
+p = inputParser;
+p.addParameter('PropertyAttributes', '', ...
+    @(x)validateattributes(x, {'char'}, {'scalartext'}, 'fillProps', 'Attribute'));
+p.parse(varargin{:});
+
 proplines = cell(size(names));
 for i=1:length(names)
     pnm = names{i};
-    property = props(pnm);
-    if ischar(property)
-        doc = ['property of type ' property];
-    elseif isa(property, 'containers.Map')
-        doc = ['reference to type ' property('target_type')];
-    elseif isstruct(property)
-        propertyNames = fieldnames(property);
-        doc = ['table with properties {' misc.cellPrettyPrint(propertyNames) '}'];
-    else
-        doc = property.doc;
-    end
-    proplines{i} = [pnm '; % ' doc];
+    proplines{i} = [pnm '; % ' getPropStr(props(pnm))];
 end
 
-if nargin >= 3
-    opt = ['(' options ')'];
+if isempty(p.Results.PropertyAttributes)
+    options = '';
 else
-    opt = '';
+    options = ['(' p.Results.PropertyAttributes ')'];
 end
 
 if isempty(proplines)
     s = '';
 else
     s = strjoin({...
-        ['properties' opt]...
+        ['properties' options]...
         file.addSpaces(strjoin(proplines, newline), 4)...
         'end'}, newline);
+end
+end
+
+function propStr = getPropStr(prop, propName)
+if ischar(prop)
+    typeStr = prop;
+elseif isstruct(prop)
+    columnNames = fieldnames(prop);
+    columnDocStr = cell(size(columnNames));
+    for i=1:length(columnNames)
+        name = columnNames{i};
+        columnDocStr{i} = getPropStr(prop.(name), name);
+    end
+    typeStr = ['Table with columns: (', strjoin(columnDocStr, ', '), ')'];
+elseif isa(prop, 'file.Attribute')
+    if isa(prop.dtype, 'containers.Map')
+        switch prop.dtype('reftype')
+            case 'region'
+                refTypeName = 'Region';
+            case 'object'
+                refTypeName = 'Object';
+            otherwise
+                error('Invalid reftype found whilst filling Constructor prop docs.');
+        end
+        typeStr = sprintf('%s Reference to %s', refTypeName, prop.dtype('target_type'));
+    else
+        typeStr = prop.dtype;
+    end
+elseif isa(prop, 'containers.Map')
+    switch prop('reftype')
+        case 'region'
+            refTypeName = 'region';
+        case 'object'
+            refTypeName = 'object';
+        otherwise
+            error('Invalid reftype found whilst filling Constructor prop docs.');
+    end
+    typeStr = sprintf('%s Reference to %s', refTypeName, prop('target_type'));
+elseif isa(prop, 'file.Dataset') && isempty(prop.type)
+    typeStr = getPropStr(prop.dtype);
+elseif isempty(prop.type)
+    typeStr = 'types.untyped.Set';
+else
+    typeStr = prop.type;
+end
+
+if isa(prop, 'file.Dataset') || isa(prop, 'file.Attribute') || isa(prop, 'file.Group')
+    docStr = prop.doc;
+else
+    docStr = '';
+end
+
+propStr = sprintf('(%s) %s', typeStr, docStr);
+
+if nargin >= 2
+    propStr = [propName ' = ' propStr];
 end
 end
