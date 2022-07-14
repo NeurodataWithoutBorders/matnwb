84979ad4e622343e6ea8faf73f602753aab4a414
Lawrence Niu <lawrence@mbfbioscience.com>
2022-07-14 12:13:49 -0400

    fix incorrect environment variable

--- +tests/+unit/dataPipeTest.m
+++ +tests/+unit/dataPipeTest.m
@@ -82,9 +82,9 @@ function testExternalFilters(testCase)
 import types.untyped.datapipe.dynamic.Filter;
 import types.untyped.datapipe.properties.DynamicFilter;
 
-error(getenv('HDF5_PLUGIN_PATH'));
+error('HDF5_PLUGIN_PATH = %s', getenv('HDF5_PLUGIN_PATH'));
 
-% testCase.assumeTrue(logical(H5Z.filter_avail(uint32(Filter.BZIP2))));
+testCase.assumeTrue(logical(H5Z.filter_avail(uint32(Filter.BZIP2))));
 
 filename = 'testBzipWrite.h5';
 
