54096648fb7cab552eedaab40352f915240e8330
Lawrence <33462079+lawrence-mbf@users.noreply.github.com>
2022-07-29 09:17:32 -0400

    Add Tutorial Tests (#450)
    * remove unused ophys script
    
    * Remove toolbox dependencies from ecephys tutorial
    
    - Not useful for someone who doesn't have the statistics toolbox
    - A lot more work to get working with CI.
    
    * Revert "Revert "Add tutorial tests""
    
    This reverts commit 5a516d08284fdbae7894f0184a2b3108fa578d7e.
    
    * Revert "Revert "Remove code blocks from dynamic filters tutorial""
    
    This reverts commit 322a2259064c53418e8720f7f630c2f6d6f1dc9d.
    
    * Revert "Revert "Update reformatted dynamic filters html""
    
    This reverts commit e8587e7cbac542923b8fb8e75f747f5dad28a257.
    
    * Fix tutorial tests missing environment files
    
    We need to generateCore here.
    
    * reinstate dynamic filter live script.
    
    * Add path before running scripts
    
    Otherwise running the scripts may not properly handle the environment correctly.
    
    * print debugging
    
    * remove formatStruct which is not a tutorial
    
    Co-authored-by: Lawrence Niu <lawrence@mbfbioscience.com>

---
+++ +tests/+unit/tutorialTest.m
@@ -0,0 +1,31 @@
+function tests = tutorialTest()
+tests = functiontests(localfunctions);
+end
+
+function setupOnce(testCase)
+rootPath = fullfile(fileparts(mfilename('fullpath')), '..', '..');
+testCase.applyFixture(matlab.unittest.fixtures.PathFixture(rootPath));
+tutorialPath = fullfile(rootPath, 'tutorials');
+addpath(tutorialPath);
+testCase.TestData.listing = dir(tutorialPath);
+end
+
+function setup(testCase)
+testCase.applyFixture(matlab.unittest.fixtures.WorkingFolderFixture);
+generateCore('savedir', '.');
+rehash();
+end
+
+function testTutorials(testCase)
+skippedTutorials = {...
+    'basicUsage.mlx', ...
+    'convertTrials.m', ...
+    'formatStruct.m'};
+for i = 1:length(testCase.TestData.listing)
+    listing = testCase.TestData.listing(i);
+    if listing.isdir || any(strcmp(skippedTutorials, listing.name))
+        continue;
+    end
+    run(listing.name);
+end
+end
