17442535a8cf1926feafa03687ab820f3091016d
Lawrence Niu <lawrence@mbfbioscience.com>
2023-05-17 13:25:28 -0400

    Add Hidden property checks to checkUnset
    Now that we have hidden properties in our generated classes.

--- +types/+util/checkUnset.m
+++ +types/+util/checkUnset.m
@@ -1,18 +1,22 @@
 function checkUnset(obj, argin)
-props = properties(obj);
-anonNames = {};
-for i = 1:length(props)
-    p = obj.(props{i});
-    if isa(p, 'types.untyped.Anon')
-        anonNames = [anonNames;{p.name}];
-    elseif isa(p, 'types.untyped.Set')
-        anonNames = [anonNames;keys(p) .'];
+    publicProperties = properties(obj);
+    objMetaClass = metaclass(obj);
+    isHiddenProperty = logical([objMetaClass.PropertyList.Hidden]);
+    hiddenProperties = {objMetaClass.PropertyList(isHiddenProperty).Name};
+    allProperties = union(publicProperties, hiddenProperties);
+    anonNames = {};
+    for i = 1:length(allProperties)
+        p = obj.(allProperties{i});
+        if isa(p, 'types.untyped.Anon')
+            anonNames = [anonNames;{p.name}];
+        elseif isa(p, 'types.untyped.Set')
+            anonNames = [anonNames;keys(p) .'];
+        end
     end
-end
-dropped = setdiff(argin, [props;anonNames]);
-assert(isempty(dropped),...
-    'NWB:CheckUnset:InvalidProperties',...
-    ['Unexpected properties {%s}.  '...
+    dropped = setdiff(argin, [allProperties;anonNames]);
+    assert(isempty(dropped),...
+        'NWB:CheckUnset:InvalidProperties',...
+        ['Unexpected properties {%s}.  '...
         '\n\nYour schema version may be incompatible with the file.  '...
         'Consider checking the schema version of the file with '...
         '`util.getSchemaVersion(filename)` '...
