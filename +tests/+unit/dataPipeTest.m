83e4cddafff3a8fa52c39bc84b9c9ae5cbeeb8f6
Lawrence Niu <lawrence@mbfbioscience.com>
2023-09-08 15:58:27 +0200

    retab functions

--- +tests/+unit/dataPipeTest.m
+++ +tests/+unit/dataPipeTest.m
@@ -1,136 +1,136 @@
 function tests = dataPipeTest()
-tests = functiontests(localfunctions);
+    tests = functiontests(localfunctions);
 end
 
 function setupOnce(testCase)
-rootPath = fullfile(fileparts(mfilename('fullpath')), '..', '..');
-testCase.applyFixture(matlab.unittest.fixtures.PathFixture(rootPath));
+    rootPath = fullfile(fileparts(mfilename('fullpath')), '..', '..');
+    testCase.applyFixture(matlab.unittest.fixtures.PathFixture(rootPath));
 end
 
 function setup(testCase)
-testCase.applyFixture(matlab.unittest.fixtures.WorkingFolderFixture);
-generateCore('savedir', '.');
-rehash();
+    testCase.applyFixture(matlab.unittest.fixtures.WorkingFolderFixture);
+    generateCore('savedir', '.');
+    rehash();
 end
 
 function testIndex(testCase)
-filename = 'testIndexing.h5';
-name = '/test_data';
+    filename = 'testIndexing.h5';
+    name = '/test_data';
 
-data = rand(100, 100, 100);
-Pipe = types.untyped.DataPipe('data', data);
+    data = rand(100, 100, 100);
+    Pipe = types.untyped.DataPipe('data', data);
 
-testCase.verifyEqual(Pipe(:), data(:));
-testCase.verifyEqual(Pipe(:,:,1), data(:,:,1));
+    testCase.verifyEqual(Pipe(:), data(:));
+    testCase.verifyEqual(Pipe(:,:,1), data(:,:,1));
 
-fid = H5F.create(filename);
-Pipe.export(fid, name, {}); % bind the pipe.
-H5F.close(fid);
+    fid = H5F.create(filename);
+    Pipe.export(fid, name, {}); % bind the pipe.
+    H5F.close(fid);
 
-testCase.verifyEqual(Pipe(:), data(:));
-testCase.verifyEqual(Pipe(:,:,1), data(:,:,1));
+    testCase.verifyEqual(Pipe(:), data(:));
+    testCase.verifyEqual(Pipe(:,:,1), data(:,:,1));
 end
 
 function testAppend(testCase)
-filename = 'testIterativeWrite.h5';
+    filename = 'testIterativeWrite.h5';
 
-Pipe = types.untyped.DataPipe(...
-    'maxSize', [10 13 15],...
-    'axis', 3,...
-    'chunkSize', [10 13 1],...
-    'dataType', 'uint8',...
-    'compressionLevel', 5);
+    Pipe = types.untyped.DataPipe(...
+        'maxSize', [10 13 15],...
+        'axis', 3,...
+        'chunkSize', [10 13 1],...
+        'dataType', 'uint8',...
+        'compressionLevel', 5);
 
-OneDimensionPipe = types.untyped.DataPipe('maxSize', Inf, 'data', [7, 8, 9]);
+    OneDimensionPipe = types.untyped.DataPipe('maxSize', Inf, 'data', [7, 8, 9]);
 
-%% create test file
-fid = H5F.create(filename);
+    %% create test file
+    fid = H5F.create(filename);
 
-initialData = createData(Pipe.dataType, [10 13 10]);
-Pipe.internal.data = initialData;
-Pipe.export(fid, '/test_data', {}); % bind
-OneDimensionPipe.export(fid, '/test_one_dim_data', {});
+    initialData = createData(Pipe.dataType, [10 13 10]);
+    Pipe.internal.data = initialData;
+    Pipe.export(fid, '/test_data', {}); % bind
+    OneDimensionPipe.export(fid, '/test_one_dim_data', {});
 
-H5F.close(fid);
+    H5F.close(fid);
 
-%% append data
-totalLength = 3;
-appendData = zeros([10 13 totalLength], Pipe.dataType);
-for i = 1:totalLength
-    appendData(:,:,i) = createData(Pipe.dataType, Pipe.chunkSize);
-    Pipe.append(appendData(:,:,i));
-end
+    %% append data
+    totalLength = 3;
+    appendData = zeros([10 13 totalLength], Pipe.dataType);
+    for i = 1:totalLength
+        appendData(:,:,i) = createData(Pipe.dataType, Pipe.chunkSize);
+        Pipe.append(appendData(:,:,i));
+    end
 
-for i = 1:totalLength
-    OneDimensionPipe.append(rand());
-end
+    for i = 1:totalLength
+        OneDimensionPipe.append(rand());
+    end
 
-%% verify data
-Pipe = types.untyped.DataPipe('filename', filename, 'path', '/test_data');
-readData = Pipe.load();
-testCase.verifyEqual(readData(:,:,1:10), initialData);
-testCase.verifyEqual(readData(:,:,11:end), appendData);
+    %% verify data
+    Pipe = types.untyped.DataPipe('filename', filename, 'path', '/test_data');
+    readData = Pipe.load();
+    testCase.verifyEqual(readData(:,:,1:10), initialData);
+    testCase.verifyEqual(readData(:,:,11:end), appendData);
 
-OneDimensionPipe = types.untyped.DataPipe('filename', filename, 'path', '/test_one_dim_data');
-readData = OneDimensionPipe.load();
-testCase.verifyTrue(isvector(readData));
-testCase.verifyEqual(length(readData), 6);
-testCase.verifyEqual(readData(1:3), [7, 8, 9] .');
+    OneDimensionPipe = types.untyped.DataPipe('filename', filename, 'path', '/test_one_dim_data');
+    readData = OneDimensionPipe.load();
+    testCase.verifyTrue(isvector(readData));
+    testCase.verifyEqual(length(readData), 6);
+    testCase.verifyEqual(readData(1:3), [7, 8, 9] .');
 end
 
 function testExternalFilters(testCase)
-import types.untyped.datapipe.dynamic.Filter;
-import types.untyped.datapipe.properties.DynamicFilter;
-import types.untyped.datapipe.properties.Shuffle;
+    import types.untyped.datapipe.dynamic.Filter;
+    import types.untyped.datapipe.properties.DynamicFilter;
+    import types.untyped.datapipe.properties.Shuffle;
 
-testCase.assumeTrue(logical(H5Z.filter_avail(uint32(Filter.LZ4))));
+    testCase.assumeTrue(logical(H5Z.filter_avail(uint32(Filter.LZ4))));
 
-filename = 'testExternalWrite.h5';
+    filename = 'testExternalWrite.h5';
 
-Pipe = types.untyped.DataPipe(...
-    'maxSize', [10 13 15],...
-    'axis', 3,...
-    'chunkSize', [10 13 1],...
-    'dataType', 'uint8',...
-    'filters', [Shuffle() DynamicFilter(Filter.LZ4)]);
+    Pipe = types.untyped.DataPipe(...
+        'maxSize', [10 13 15],...
+        'axis', 3,...
+        'chunkSize', [10 13 1],...
+        'dataType', 'uint8',...
+        'filters', [Shuffle() DynamicFilter(Filter.LZ4)]);
 
-OneDimensionPipe = types.untyped.DataPipe('maxSize', Inf, 'data', [7, 8, 9]);
+    OneDimensionPipe = types.untyped.DataPipe('maxSize', Inf, 'data', [7, 8, 9]);
 
-%% create test file
-fid = H5F.create(filename);
+    %% create test file
+    fid = H5F.create(filename);
 
-initialData = createData(Pipe.dataType, [10 13 10]);
-Pipe.internal.data = initialData;
-Pipe.export(fid, '/test_data', {}); % bind
-OneDimensionPipe.export(fid, '/test_one_dim_data', {});
+    initialData = createData(Pipe.dataType, [10 13 10]);
+    Pipe.internal.data = initialData;
+    Pipe.export(fid, '/test_data', {}); % bind
+    OneDimensionPipe.export(fid, '/test_one_dim_data', {});
 
-H5F.close(fid);
+    H5F.close(fid);
 
-%% append data
-totalLength = 3;
-appendData = zeros([10 13 totalLength], Pipe.dataType);
-for i = 1:totalLength
-    appendData(:,:,i) = createData(Pipe.dataType, Pipe.chunkSize);
-    Pipe.append(appendData(:,:,i));
-end
+    %% append data
+    totalLength = 3;
+    appendData = zeros([10 13 totalLength], Pipe.dataType);
+    for i = 1:totalLength
+        appendData(:,:,i) = createData(Pipe.dataType, Pipe.chunkSize);
+        Pipe.append(appendData(:,:,i));
+    end
 
-for i = 1:totalLength
-    OneDimensionPipe.append(rand());
-end
+    for i = 1:totalLength
+        OneDimensionPipe.append(rand());
+    end
 
-%% verify data
-Pipe = types.untyped.DataPipe('filename', filename, 'path', '/test_data');
-readData = Pipe.load();
-testCase.verifyEqual(readData(:,:,1:10), initialData);
-testCase.verifyEqual(readData(:,:,11:end), appendData);
+    %% verify data
+    Pipe = types.untyped.DataPipe('filename', filename, 'path', '/test_data');
+    readData = Pipe.load();
+    testCase.verifyEqual(readData(:,:,1:10), initialData);
+    testCase.verifyEqual(readData(:,:,11:end), appendData);
 
-OneDimensionPipe = types.untyped.DataPipe('filename', filename, 'path', '/test_one_dim_data');
-readData = OneDimensionPipe.load();
-testCase.verifyTrue(isvector(readData));
-testCase.verifyEqual(length(readData), 6);
-testCase.verifyEqual(readData(1:3), [7, 8, 9] .');
+    OneDimensionPipe = types.untyped.DataPipe('filename', filename, 'path', '/test_one_dim_data');
+    readData = OneDimensionPipe.load();
+    testCase.verifyTrue(isvector(readData));
+    testCase.verifyEqual(length(readData), 6);
+    testCase.verifyEqual(readData(1:3), [7, 8, 9] .');
 end
 
 function data = createData(dataType, size)
-data = randi(intmax(dataType), size, dataType);
+    data = randi(intmax(dataType), size, dataType);
 end
--- +types/+util/+dynamictable/getIndex.m
+++ +types/+util/+dynamictable/getIndex.m
@@ -1,73 +1,72 @@
-function indexName = getIndex(DynamicTable, column)
-%GETINDEX Given a dynamic table and its column name, get its VectorIndex column name
-validateattributes(DynamicTable,...
-    {'types.core.DynamicTable', 'types.hdmf_common.DynamicTable'},...
-    {'scalar'});
-validateattributes(column, {'char'}, {'scalartext'});
-indexName = '';
-if strcmp(column, 'id')
-    return;
-end
+function indexName = getIndex(DynamicTable, columnName)
+    %GETINDEX Given a dynamic table and its column name, get its VectorIndex column name
+    validateattributes(DynamicTable,...
+        {'types.core.DynamicTable', 'types.hdmf_common.DynamicTable'},...
+        {'scalar'});
+    validateattributes(columnName, {'char'}, {'scalartext'});
+    indexName = '';
+    if strcmp(columnName, 'id')
+        return;
+    end
 
-% after Schema version 2.3.0, VectorIndex objects subclass VectorData which
-% meant that vectorindex and vectordata sets could be combined.
-isLegacyDynamicTable = isprop(DynamicTable, 'vectorindex');
-if isLegacyDynamicTable
-    vecKeys = keys(DynamicTable.vectorindex);
-else
-    vecKeys = keys(DynamicTable.vectordata);
-end
-for i = 1:length(vecKeys)
-    vk = vecKeys{i};
+    % after Schema version 2.3.0, VectorIndex objects subclass VectorData which
+    % meant that vectorindex and vectordata sets could be combined.
+    isLegacyDynamicTable = isprop(DynamicTable, 'vectorindex');
     if isLegacyDynamicTable
-        vecData = DynamicTable.vectorindex.get(vk);
+        vectorKeys = keys(DynamicTable.vectorindex);
     else
-        vecData = DynamicTable.vectordata.get(vk);
+        vectorKeys = keys(DynamicTable.vectordata);
     end
-    if ~isa(vecData, 'types.hdmf_common.VectorIndex')...
-            && ~isa(vecData, 'types.core.VectorIndex')
-        continue;
+    for iKey = 1:length(vectorKeys)
+        tableProperties = vectorKeys{iKey};
+        if isLegacyDynamicTable
+            Vector = DynamicTable.vectorindex.get(tableProperties);
+        else
+            Vector = DynamicTable.vectordata.get(tableProperties);
+        end
+        if ~isa(Vector, 'types.hdmf_common.VectorIndex') && ~isa(Vector, 'types.core.VectorIndex')
+            continue;
+        end
+        if isVecIndColumn(DynamicTable, Vector, columnName)
+            indexName = tableProperties;
+            return;
+        end
     end
-    if isVecIndColumn(DynamicTable, vecData, column)
-        indexName = vk;
-        return;
-    end
-end
 
-% check if dynamic table object has extended properties which point to
-% vector indices. These are specifically defined by the schema to be
-% properties.
-DynamicTableProps = properties(DynamicTable);
-isPropVecInd = false(size(DynamicTableProps));
-for i = 1:length(DynamicTableProps)
-    PropVec = DynamicTable.(DynamicTableProps{i});
-    isPropVecInd(i) = isa(PropVec, 'types.hdmf_common.VectorIndex')...
-        || isa(PropVec, 'types.core.VectorIndex');
-end
-
-DynamicTableProps = DynamicTableProps(isPropVecInd);
-for i = 1:length(DynamicTableProps)
-    vk = DynamicTableProps{i};
-    VecInd = DynamicTable.(vk);
-    if isVecIndColumn(DynamicTable, VecInd, column)
-        indexName = vk;
-        return;
+    % check if dynamic table object has extended properties which point to
+    % vector indices. These are specifically defined by the schema to be
+    % properties.
+    DynamicTableProps = properties(DynamicTable);
+    isPropVecInd = false(size(DynamicTableProps));
+    for i = 1:length(DynamicTableProps)
+        PropVec = DynamicTable.(DynamicTableProps{i});
+        isPropVecInd(i) = isa(PropVec, 'types.hdmf_common.VectorIndex')...
+            || isa(PropVec, 'types.core.VectorIndex');
     end
-end
+
+    DynamicTableProps = DynamicTableProps(isPropVecInd);
+    for i = 1:length(DynamicTableProps)
+        tableProperties = DynamicTableProps{i};
+        VecInd = DynamicTable.(tableProperties);
+        if isVecIndColumn(DynamicTable, VecInd, columnName)
+            indexName = tableProperties;
+            return;
+        end
+    end
 end
 
 function tf = isVecIndColumn(DynamicTable, VectorIndex, column)
-if VectorIndex.target.has_path()
-    tf = endsWith(VectorIndex.target.path, ['/' column]);
-elseif isprop(DynamicTable, column)
-    tf = VectorIndex.target.target == DynamicTable.(column);
-else
-    if isprop(DynamicTable, 'vectorindex') && DynamicTable.vectorindex.isKey(column)
-        Vec = DynamicTable.vectorindex.get(column);
+    if VectorIndex.target.has_path()
+        tf = endsWith(VectorIndex.target.path, ['/' column]);
+    elseif isprop(DynamicTable, column)
+        tf = VectorIndex.target.target == DynamicTable.(column);
     else
-        Vec = DynamicTable.vectordata.get(column);
+        if isprop(DynamicTable, 'vectorindex') && DynamicTable.vectorindex.isKey(column)
+            Vec = DynamicTable.vectorindex.get(column);
+        else
+            Vec = DynamicTable.vectordata.get(column);
+        end
+        tf = VectorIndex.target.target == Vec;
     end
-    tf = VectorIndex.target.target == Vec;
-end
 end
 
