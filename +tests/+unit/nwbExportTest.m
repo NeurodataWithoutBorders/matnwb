da1ec61ac653027f095d51d8a8b625dc6918f05c
ehennestad <ehennestad@gmail.com>
2024-09-22 11:04:34 +0200

    Add unittest to test changes made in this PR

---
+++ +tests/+unit/nwbExportTest.m
@@ -0,0 +1,63 @@
+classdef nwbExportTest < matlab.unittest.TestCase
+
+    properties
+        NwbObject
+        OutputFolder = "out"
+    end
+
+    methods (TestClassSetup)
+        function setupClass(testCase)
+            % Get the root path of the matnwb repository
+            rootPath = misc.getMatnwbDir();
+
+            % Use a fixture to add the folder to the search path
+            testCase.applyFixture(matlab.unittest.fixtures.PathFixture(rootPath));
+
+            % Use a fixture to create a temporary working directory
+            testCase.applyFixture(matlab.unittest.fixtures.WorkingFolderFixture);
+            generateCore('savedir', '.');
+        end
+    end
+
+    methods (TestMethodSetup)
+        function setupMethod(testCase)
+            testCase.NwbObject = testCase.initNwbFile();
+
+            if isfolder( testCase.OutputFolder )
+                rmdir(testCase.OutputFolder, "s")
+            end
+            mkdir(testCase.OutputFolder)
+        end
+    end
+
+    methods (Test)
+        function testExportNwbFileWithMissingRequiredProperties(testCase)
+            nwb = NwbFile();
+            nwbFilePath = fullfile(testCase.OutputFolder, 'testfile.nwb');
+            testCase.verifyError(@(file, path) nwbExport(nwb, nwbFilePath), ...
+                'NWB:RequiredPropertyMissing')
+        end
+
+        function testExportTimeseriesWithMissingTimestampsAndStartingTime(testCase)
+            time_series = types.core.TimeSeries( ...
+                 'data', linspace(0, 0.4, 50), ...
+                 'description', 'a test series', ...
+                 'data_unit', 'n/a' ...
+             );
+
+             testCase.NwbObject.acquisition.set('time_series', time_series);
+             nwbFilePath = fullfile(testCase.OutputFolder, 'testfile.nwb');
+             testCase.verifyError(@(f, fn) nwbExport(testCase.NwbObject, nwbFilePath), ...
+                 'NWB:CustomConstraintUnfulfilled')
+        end
+    end
+
+    methods (Static)
+        function nwb = initNwbFile()
+            nwb = NwbFile( ...
+                'session_description', 'test file for nwb export', ...
+                'identifier', 'export_test', ...
+                'session_start_time', datetime("now", 'TimeZone', 'local') );
+        end
+    end
+end
