e5cf9b915177abaa157dd9d9e2540dda6bcaab90
ehennestad <ehennestad@gmail.com>
2024-11-02 15:25:58 +0100

    Fix test for special case dataset starting_time in TimeSeries

--- +tests/+unit/nwbExportTest.m
+++ +tests/+unit/nwbExportTest.m
@@ -50,7 +50,7 @@ classdef nwbExportTest < matlab.unittest.TestCase
              testCase.verifyError(@(f, fn) nwbExport(testCase.NwbObject, nwbFilePath), ...
                  'NWB:CustomConstraintUnfulfilled')
         end
-        
+
         function testExportDependentAttributeWithMissingParentA(testCase)
             testCase.NwbObject.general_source_script_file_name = 'my_test_script.m';
             nwbFilePath = fullfile(testCase.OutputFolder, 'test_part1.nwb');
@@ -62,22 +62,17 @@ classdef nwbExportTest < matlab.unittest.TestCase
             testCase.verifyWarningFree(@(f, fn) nwbExport(testCase.NwbObject, nwbFilePath))
         end
 
-        function testExportDependentAttributeWithMissingParentB(testCase)
+        function testExportTimeseriesWithoutStartingTimeRate(testCase)
             time_series = types.core.TimeSeries( ...
                 'data', linspace(0, 0.4, 50), ...
-                'starting_time_rate', 10.0, ...
+                'starting_time', 1, ...
                 'description', 'a test series', ...
                 'data_unit', 'n/a' ...
             );
 
             testCase.NwbObject.acquisition.set('time_series', time_series);
             nwbFilePath = fullfile(testCase.OutputFolder, 'test_part1.nwb');
-            testCase.verifyWarning(@(f, fn) nwbExport(testCase.NwbObject, nwbFilePath), 'NWB:DependentAttributeNotExported')
-
-            % Add value for dataset which attribute depends on and export again
-            time_series.starting_time = 1;
-            nwbFilePath = fullfile(testCase.OutputFolder, 'test_part2.nwb');
-            testCase.verifyWarningFree(@(f, fn) nwbExport(testCase.NwbObject, nwbFilePath))
+            testCase.verifyError(@(f, fn) nwbExport(testCase.NwbObject, nwbFilePath), 'NWB:CustomConstraintUnfulfilled')
         end
     end
 
