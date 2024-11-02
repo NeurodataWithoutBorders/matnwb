271e0f8a69931ed535894b80cfb2802ec527a3e2
ehennestad <ehennestad@gmail.com>
2024-11-02 15:56:35 +0100

    unrelated: provoke test failure to check workflow behavior

--- +tests/+unit/nwbExportTest.m
+++ +tests/+unit/nwbExportTest.m
@@ -69,7 +69,7 @@ classdef nwbExportTest < matlab.unittest.TestCase
                 'description', 'a test series', ...
                 'data_unit', 'n/a' ...
             );
-            error('') % Temp: provoke error
+            tesCase.verifyFail('') % Temp: provoke error
             testCase.NwbObject.acquisition.set('time_series', time_series);
             nwbFilePath = fullfile(testCase.OutputFolder, 'test_part1.nwb');
             testCase.verifyError(@(f, fn) nwbExport(testCase.NwbObject, nwbFilePath), 'NWB:CustomConstraintUnfulfilled')
