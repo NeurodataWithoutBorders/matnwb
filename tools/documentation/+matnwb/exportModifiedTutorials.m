34023febd4ef691183a190a2633f931a5718c326
ehennestad <ehennestad@gmail.com>
2024-11-04 08:49:45 +0100

    Add functions for exporting tutorials

---
+++ tools/documentation/+matnwb/exportModifiedTutorials.m
@@ -0,0 +1,33 @@
+function exportModifiedTutorials()
+
+    if exist("isMATLABReleaseOlderThan", "file") == 2
+        hasGitRepo = ~isMATLABReleaseOlderThan("R2023b");
+    else
+        hasGitRepo = false;
+    end
+    
+    if hasGitRepo
+        repo = gitrepo(misc.getMatnwbDir);
+        modifiedFiles = repo.ModifiedFiles;
+    else
+        currentDir = pwd;
+        cleanupObj = onCleanup(@(fp) cd(currentDir));
+        cd(misc.getMatnwbDir)
+        [status, cmdout] = system('git --no-pager diff --name-only');
+        clear cleanupObj
+        if status == 0
+            modifiedFiles = splitlines(cmdout);
+            modifiedFiles = string(modifiedFiles);
+            modifiedFiles(modifiedFiles=="") = [];
+            modifiedFiles = fullfile(misc.getMatnwbDir, modifiedFiles);
+        else
+            error('Could not use git to detect modified files.')
+        end
+    end
+
+    isTutorialFile = startsWith(modifiedFiles, fullfile(misc.getMatnwbDir, 'tutorials'));
+    isTutorialFile = isTutorialFile & endsWith(modifiedFiles, ".mlx");
+    tutorialFiles = modifiedFiles(isTutorialFile);
+    
+    matnwb.exportTutorials("FilePaths", tutorialFiles)
+end
---
+++ tools/documentation/+matnwb/exportTutorials.m
@@ -0,0 +1,72 @@
+function exportTutorials(options)
+% exportTutorials - Export tutorial mlx files the specified output format
+
+% see also fullfile(matlabroot, 'toolbox/matlab/codetools/+matlab/+internal')
+
+    arguments
+        options.ExportFormat (1,:) string {mustStartWithDot} = [".m", ".html"]
+        options.Expression (1,1) string = "*" % Filter by expression
+        options.FileNames (1,:) string = string.empty % Filter by file names
+        options.FilePaths (1,:) string = string.empty % Export specified files
+    end
+    
+    [exportFormat, targetFolderNames] = deal(options.ExportFormat);
+
+    targetFolderNames = extractAfter(targetFolderNames, ".");
+    targetFolderNames(strcmp(targetFolderNames, "m")) = "mcode";
+
+    nwbTutorialDir = fullfile(misc.getMatnwbDir, "tutorials");
+    targetFolderPaths = fullfile(nwbTutorialDir, targetFolderNames);
+
+    for folderPath = targetFolderPaths
+        if ~isfolder(folderPath); mkdir(folderPath); end
+    end
+    
+    if isempty(options.FilePaths)
+        if endsWith(options.Expression, "*")
+            expression = options.Expression + ".mlx";
+        else
+            expression = options.Expression + "*.mlx";
+        end
+    
+        L = dir(fullfile(nwbTutorialDir, expression));
+        filePaths = string( fullfile({L.folder}, {L.name}) );
+    else
+        filePaths = options.FilePaths;
+    end
+
+    [~, fileNames] = fileparts(filePaths);
+    if ~isempty(options.FileNames)
+        [fileNames, iA] = intersect(fileNames, options.FileNames, 'stable');
+        filePaths = filePaths(iA);
+    end
+
+    % Go to a temporary directory, so that tutorials are exported in a
+    % temporary folder which is cleaned up afterwards
+    currentDir = pwd();
+    cleanupWorkdir = onCleanup(@(fp) cd(currentDir));
+
+    tempDir = fullfile(tempdir, 'nwbTutorials');
+    if ~isfolder(tempDir); mkdir(tempDir); end
+    cd(tempDir)
+
+    cleanupDeleteTempFiles = onCleanup(@(fp) rmdir(tempDir, 's'));
+    disp(tempDir)
+
+    for i = 1:numel(filePaths)
+        sourcePath = char( fullfile(filePaths(i)) );
+        matlab.internal.liveeditor.executeAndSave(sourcePath);
+
+        for j = 1:numel(exportFormat)
+            targetPath = fullfile(targetFolderPaths(j), fileNames(i) + exportFormat(j));
+            export(sourcePath, strrep(targetPath, '.mlx', exportFormat(j)));
+        end
+    end
+end
+
+function mustStartWithDot(value)
+    for i = 1:numel(value)
+        assert(startsWith(value(i), '.'), ...
+            'Value must be a file extension starting with a period, e.g ".html"')
+    end
+end
