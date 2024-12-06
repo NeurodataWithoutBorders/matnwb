09d464c8d0d2fdacc5e9df8008e2f61b0bc65387
ehennestad <ehennestad@gmail.com>
2024-12-06 12:32:52 +0100

    Update main function docstrings to work better with sphinx/rst formatting
    Add "sections"
    Specify input and output arguments
    Reformat examples

--- generateCore.m
+++ generateCore.m
@@ -1,27 +1,40 @@
 function generateCore(version, options)
-    % GENERATECORE Generate Matlab classes from NWB core schema files
-    %   GENERATECORE()  Generate classes (Matlab m-files) from the
-    %   NWB core namespace file. By default, generates off of the most recent nwb-schema
-    %   release.
-    %
-    %   GENERATECORE(version)  Generate classes for the
-    %   core namespace of the listed version.
-    %
-    %   A cache of schema data is generated in the 'namespaces' subdirectory in
-    %   the current working directory.  This is for allowing cross-referencing
-    %   classes between multiple namespaces.
-    %
-    %   Output files are generated placed in a '+types' subdirectory in the
-    %   current working directory.
-    %
-    %   GENERATECORE(__, 'savedir', saveDirectory) Generates the core class
-    %   files in the specified directory.
-    %
-    %   Example:
-    %      generateCore();
-    %      generateCore('2.2.3');
-    %
-    %   See also GENERATEEXTENSION
+% GENERATECORE - Generate Matlab classes from NWB core schema files
+%
+% Syntax:
+%   GENERATECORE() Generate classes (Matlab m-files) from the
+%   NWB core namespace file. By default, generates off of the most recent 
+%   nwb-schema release.
+%
+%   GENERATECORE(version)  Generate classes for the
+%   core namespace of the listed version.
+%
+%   GENERATECORE(__, Name, Value)  Generate classes based on optional 
+%   name-value pairs controlling the output .
+%
+%   A cache of schema data is generated in the ``namespaces`` subdirectory in
+%   the matnwb root directory.  This is for allowing cross-referencing
+%   classes between multiple namespaces.
+%
+%   Output files are placed in a ``+types`` subdirectory in the
+%   matnwb root directory directory.
+%
+% Usage:
+%  Example 1 - Generate core schemas for the latest version of NWB::
+%
+%    generateCore();
+%
+%  Example 2 - Generate core schemas for an older version of NWB::
+%
+%     generateCore('2.2.3');
+%
+%  Example 3 - Generate and save classes in a custom location::
+%
+%     %  Generates the core class files in the specified directory.
+%     generateCore('savedir', saveDirectory)
+%
+% See also:
+%   generateExtension
 
     arguments
         version (1,1) string {matnwb.common.mustBeValidSchemaVersion} = "latest"
--- nwbRead.m
+++ nwbRead.m
@@ -1,21 +1,53 @@
 function nwb = nwbRead(filename, flags, options)
-    %NWBREAD Reads an NWB file.
-    %  nwb = NWBREAD(filename) Reads the nwb file at filename and returns an
-    %  NWBFile object representing its contents.
-    %  nwb = nwbRead(filename, 'ignorecache') Reads the nwb file without generating classes
-    %  off of the cached schema if one exists.
-    %
-    %  nwb = NWBREAD(filename, options)
-    %
-    %  Requires that core and extension NWB types have been generated
-    %  and reside in a 'types' package on the matlab path.
-    %
-    %  Example:
-    %    nwb = nwbRead('data.nwb');
-    %    nwb = nwbRead('data.nwb', 'ignorecache');
-    %    nwb = nwbRead('data.nwb', 'savedir', '.');
-    %
-    %  See also GENERATECORE, GENERATEEXTENSION, NWBFILE, NWBEXPORT
+% NWBREAD - Read an NWB file.
+%
+% Syntax:
+%  nwb = NWBREAD(filename) Reads the nwb file at filename and returns an
+%  NWBFile object representing its contents.
+%
+%  nwb = NWBREAD(filename, flags) Reads the nwb file using optional 
+%  flags controlling the mode for how to read the file. See input
+%  arguments for a list of available flags.
+%
+%  nwb = NWBREAD(filename, Name, Value) Reads the nwb file using optional 
+%  name-value pairs controlling options for how to read the file.
+%
+% Input Arguments:
+%  - filename (string) - 
+%    Filepath pointing to an NWB file.
+% 
+%  - flags (string) -
+%    Flag for setting the mode for the NWBREAD operation. Available options are:
+%    'ignorecache'. If the 'ignorecache' flag is used, classes for NWB data types
+%    are not re-generated based on the embedded schemas in the file.
+% 
+%  - options (name-value pairs) -
+%    Optional name-value pairs. Available options:
+%  
+%    - savedir (string) -
+%      A folder to save generated classes for NWB types.
+% 
+% Output Arguments:
+%  - nwb (NwbFile) - Nwb file object
+%
+% Usage:
+%  Example 1 - Read an NWB file::
+%
+%    nwb = nwbRead('data.nwb');
+%
+%  Example 2 - Read an NWB file without re-generating classes for NWB types::
+%
+%    nwb = nwbRead('data.nwb', 'ignorecache');
+%
+%  Note: This is a good option to use if you are reading several files
+%  which are created of the same version of the NWB schemas.
+%
+%  Example 3 - Read an NWB file and generate classes for NWB types in the current working directory::
+%
+%    nwb = nwbRead('data.nwb', 'savedir', '.');
+%
+% See also:
+%   generateCore, generateExtension, NwbFile, nwbExport
     
     arguments
         filename (1,1) string {matnwb.common.mustBeNwbFile}
