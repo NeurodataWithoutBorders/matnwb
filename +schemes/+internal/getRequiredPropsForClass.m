ce31aa1bc6855a5fc8f4ff5533089e288438c03b
ehennestad <ehennestad@gmail.com>
2025-03-20 22:56:39 +0100

    Update getRequiredPropsForClass.m
    Resolve property attributes across the whole class hierarchy, and add some comments

--- +schemes/+internal/getRequiredPropsForClass.m
+++ +schemes/+internal/getRequiredPropsForClass.m
@@ -1,21 +1,38 @@
 function requiredPropertyNames = getRequiredPropsForClass(fullClassName)
+% getRequiredPropsForClass - List required properties for a neurodata type / class
+
     arguments
-        fullClassName (1,1) string
+        fullClassName (1,1) string % E.g types.core.TimeSeries
     end
 
+    % For the NwbFile class, we need to replace it with the generated
+    % NWBFile superclass in order to retrieve schema information correctly
     if strcmp(fullClassName, 'NwbFile')
         superclassNames = string(superclasses(fullClassName));
         fullClassName = superclassNames(endsWith(superclassNames, 'NWBFile'));
     end
 
+    % Load cached namespace specifications
     classNameSplit = strsplit(fullClassName, '.');
     className = classNameSplit(end);
     namespaceName = classNameSplit(find(strcmp(classNameSplit, 'types'))+1);
     namespaceName = strrep(namespaceName, '_', '-');
     namespace = schemes.loadNamespace(namespaceName);
    
-    [~, classprops, ~] = file.processClass(className, namespace, containers.Map);
-
+    % Process/parse the namespace specifications to retrieve attributes for
+    % the class properties.
+    processedTypeMap = containers.Map;
+    [processed, classprops, ~] = file.processClass(className, namespace, processedTypeMap);
+    if ~isempty(processed)
+        superClassProps = cell(1, numel(processed)-1);
+        for iSuper = 2:numel(processed)
+            [~, superClassProps{iSuper-1}, ~] =  file.processClass(processed(iSuper).type, namespace, processedTypeMap);
+        end
+        classprops = file.internal.mergeProps(classprops, superClassProps);
+    end
+    
+    % Resolve the required properties. For the final list of required properties,
+    % we ignore both hidden and read-only properties.
     allPropertieNames = keys(classprops);
     [isRequired, isReadOnly, isHidden] = deal( false(1, classprops.Count) );
     
