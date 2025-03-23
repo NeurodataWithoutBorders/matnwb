f3014b1e20486838da9dc9663617ec219b0bb3e8
ehennestad <ehennestad@gmail.com>
2025-03-23 08:48:13 +0100

    Move some tests from tests.unit.FunctionTests to tests.unit.io.WriteTest

--- +tests/+unit/+io/WriteTest.m
+++ +tests/+unit/+io/WriteTest.m
@@ -86,6 +86,32 @@ classdef WriteTest < matlab.unittest.TestCase
             parsedData = table2struct( struct2table(parsedData) )';
             testCase.verifyEqual(data, parsedData);
         end
+        
+        function testWriteCompoundMap(testCase)
+            testCase.applyFixture(matlab.unittest.fixtures.WorkingFolderFixture)
+            fid = H5F.create('test.h5');
+            data = containers.Map({'a', 'b'}, 1:2);
+            io.writeCompound(fid, '/map_data', data)
+            H5F.close(fid);
+        end
+        
+        function testWriteCompoundEmpty(testCase)
+            testCase.applyFixture(matlab.unittest.fixtures.WorkingFolderFixture)
+            fid = H5F.create('test.h5');
+            data = struct;
+            testCase.verifyError(...
+                @(varargin) io.writeCompound(fid, '/map_data', data), ...
+                'MATLAB:imagesci:hdf5lib:libraryError')
+            H5F.close(fid);
+        end
+        
+        function testWriteCompoundScalar(testCase)
+            testCase.applyFixture(matlab.unittest.fixtures.WorkingFolderFixture)
+            fid = H5F.create('test.h5');
+            data = struct('a','b');
+            io.writeCompound(fid, '/map_data', data)
+            H5F.close(fid);
+        end
 
         function testWriteCompoundOverWrite(testCase)
                    
