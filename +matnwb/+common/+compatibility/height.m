4cbfd0b7297a8ade1738df711f6c0b94364ae346
ehennestad <ehennestad@gmail.com>
2025-03-31 20:46:14 +0200

    Update height.m
    Should also work for cell array

--- +matnwb/+common/+compatibility/height.m
+++ +matnwb/+common/+compatibility/height.m
@@ -3,7 +3,7 @@ function result = height(value)
 %
 % From R2020b: height returns the number of rows of an array
 
-    if verLessThan('matlab', '9.9') && isnumeric(value) %#ok<VERLESSMATLAB> - MATLAB < R2020b
+    if verLessThan('matlab', '9.9') && (isnumeric(value) || iscell(value)) %#ok<VERLESSMATLAB> - MATLAB < R2020b
         valueSize = size(value);
         result = valueSize(1);
     else
