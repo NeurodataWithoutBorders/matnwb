54c5bc7c969142fe7e4517ff05bb2b4c0ea4ae4f
ehennestad <ehennestad@gmail.com>
2025-04-02 01:11:41 +0200

    Revert "Try: Possibly a bug"
    This reverts commit 3c78c193f01fb47ca13f39ad69a87e63465a3573.

--- +io/mapData2H5.m
+++ +io/mapData2H5.m
@@ -43,7 +43,7 @@ elseif ~forceArray && ~iscell(data) && isscalar(data)
 elseif ~forceChunked && isempty(data)
     sid = H5S.create_simple(1, 0, 0);
 else
-    if (isvector(data) || isempty(data)) && ~forceArray
+    if isvector(data) || isempty(data)
         num_dims = 1;
         dims = length(data);
     else
