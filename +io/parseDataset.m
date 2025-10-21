b5d624306bee3d5073d3ffb5dd50e9c744c1364c
ehennestad <ehennestad@gmail.com>
2025-10-21 15:48:16 +0200

    Handle compound HDF5 types in datatypeInfoToMatlabType
    Added extractCompoundTypeDescriptor to support parsing HDF5 compound types into MATLAB struct type descriptors. This enables correct handling of nested compound, string, enum, and reference types when converting HDF5 datatypes to MATLAB representations.

--- +io/parseDataset.m
+++ +io/parseDataset.m
@@ -89,6 +89,7 @@ function matlabDataType = datatypeInfoToMatlabType(datatype, info)
     
     % This function is not exhaustive, and might not be able to resolve
     % every type. If type is not detected, output will be an empty char.
+    % For compound types, returns a struct (type descriptor)
 
     matlabDataType = '';
 
@@ -103,7 +104,8 @@ function matlabDataType = datatypeInfoToMatlabType(datatype, info)
                 matlabDataType = 'char';
             end
         elseif strcmp(datatype.Class, 'H5T_COMPOUND')
-            matlabDataType = 'table';
+            % Extract compound type descriptor from h5info structure
+            matlabDataType = extractCompoundTypeDescriptor(datatype, info);
         elseif strcmp(datatype.Class, 'H5T_ENUM')
             if io.isBool(datatype.Type)
                 matlabDataType = 'logical';
@@ -113,6 +115,70 @@ function matlabDataType = datatypeInfoToMatlabType(datatype, info)
                     'Will be saved as cell array of characters.'], ...
                     info.Name, length(datatype.Type.Member));
             end
+        end
+    end
+end
+
+function typeDescriptor = extractCompoundTypeDescriptor(datatype, info)
+%EXTRACTCOMPOUNDTYPEDESCRIPTOR Extract type descriptor from h5info datatype struct
+%   Creates a struct where each field corresponds to a compound member
+%   and the value is the MATLAB type string for that member.
+
+    typeDescriptor = struct();
+    
+    if isfield(datatype.Type, 'Member') && ~isempty(datatype.Type.Member)
+        members = datatype.Type.Member;
+        
+        for i = 1:length(members)
+            memberName = members(i).Name;
+            
+            % Determine MATLAB type for this member
+            if ischar(members(i).Datatype)
+                memberType = io.getMatType(members(i).Datatype);
+            elseif isstruct(members(i).Datatype)
+                % Handle nested or complex types
+                if strcmp(members(i).Datatype.Class, 'H5T_COMPOUND')
+                    % Recursively handle nested compound types
+                    memberType = extractCompoundTypeDescriptor(members(i), info);
+                elseif strcmp(members(i).Datatype.Class, 'H5T_STRING')
+                    memberType = 'char';
+                elseif strcmp(members(i).Datatype.Class, 'H5T_ENUM')
+                    if isfield(members(i).Datatype, 'Type') && io.isBool(members(i).Datatype.Type)
+                        memberType = 'logical';
+                    else
+                        memberType = 'char';  % Unknown enum as char
+                    end
+                elseif strcmp(members(i).Datatype.Class, 'H5T_REFERENCE')
+                    % Distinguish between ObjectView and RegionView reference types
+                    % h5info provides a Type field that indicates the specific reference type:
+                    %   'H5R_OBJECT' → ObjectView (object reference)
+                    %   'H5R_DATASET_REGION' → RegionView (region reference)
+                    if isfield(members(i).Datatype, 'Type')
+                        if strcmp(members(i).Datatype.Type, 'H5R_OBJECT')
+                            memberType = 'types.untyped.ObjectView';
+                        elseif strcmp(members(i).Datatype.Type, 'H5R_DATASET_REGION')
+                            memberType = 'types.untyped.RegionView';
+                        else
+                            % Unknown reference type - default to ObjectView
+                            error('NWB:ParseDataset:UnknownReferenceType', ...
+                                'Unknown reference type ''%s'' in field ''%s''.', ...
+                                members(i).Datatype.Type, memberName);
+                        end
+                    else
+                        % No type information available - default to ObjectView
+                        error('NWB:ParseDataset:MissingReferenceType', ...
+                            'Reference type information missing for field ''%s''.', ...
+                            memberName);
+                    end
+                else
+                    memberType = 'unknown';
+                end
+            else
+                memberType = 'unknown';
+            end
+            
+            % Build type descriptor
+            typeDescriptor.(memberName) = memberType;
         end
     end
 end
