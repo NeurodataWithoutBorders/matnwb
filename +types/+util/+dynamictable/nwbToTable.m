180fe1a7cf8ad3b852ca2a9dbf08cacb7fa685fd
ehennestad <ehennestad@gmail.com>
2026-04-07 22:00:23 +0200

    Clean whitespace and indentation

--- +types/+util/+dynamictable/nwbToTable.m
+++ +types/+util/+dynamictable/nwbToTable.m
@@ -1,13 +1,13 @@
 function matlabTable = nwbToTable(DynamicTable, index)
-%NWBTOTABLE converts from a NWB DynamicTable to a MATLAB table 
+%NWBTOTABLE converts from a NWB DynamicTable to a MATLAB table
 %
 %   MATLABTABLE = NWBTOTABLE(T) converts object T of class types.core.DynamicTable
 %   into a MATLAB Table
-%   
+%
 %   MATLABTABLE = NWBTOTABLE(T, INDEX) If INDEX is FALSE, includes rows referenced by a
 %   DynamicTableRegion as nested subtables
 %
-% EXAMPLE 
+% EXAMPLE
 % MYTABLE = types.hdmf_common.DynamicTable( ...
 %     'description','an example table', ...
 %     'colnames', {'col1','col2'}, ...
@@ -23,71 +23,72 @@ function matlabTable = nwbToTable(DynamicTable, index)
 % );
 % MATLABTABLE = nwb2table(MYTABLE);
 
-%make sure input is dynamic table
-validateattributes(DynamicTable,...
-    {'types.core.DynamicTable', 'types.hdmf_common.DynamicTable'},...
-    {'scalar'});
-
-if nargin < 2
-    index = true;
-end
-
-if isempty(DynamicTable.id)
-    matlabTable = table({}, 'VariableNames', [{'id'} DynamicTable.colnames]);
-    return;
-end
-
-% initialize table with id column
-if isa(DynamicTable.id.data, 'types.untyped.DataStub')...
-        || isa(DynamicTable.id.data, 'types.untyped.DataPipe')
-    ids = DynamicTable.id.data.load();
-else
-    ids = DynamicTable.id.data;
-end
-matlabTable = table( ...
-    ids, ...
-    'VariableNames', {'id'} ...
-);
-
-% deal with DynamicTableRegion columns when index is false
-[columns, remainingColumns] = deal(DynamicTable.colnames);
-columnDescriptions = repmat({''}, 1, length(columns));
-
-for i = 1:length(columns)
-    cn = columns{i};
-    if isprop(DynamicTable, cn)
-        cv = DynamicTable.(cn);
-    elseif isprop(DynamicTable, 'vectorindex') && DynamicTable.vectorindex.isKey(cn) % Schema version < 2.3.0
-        cv = DynamicTable.vectorindex.get(cn);
-    else
-        cv = DynamicTable.vectordata.get(cn);
+    % make sure input is dynamic table
+    validateattributes(DynamicTable,...
+        {'types.core.DynamicTable', 'types.hdmf_common.DynamicTable'},...
+        {'scalar'});
+    
+    if nargin < 2
+        index = true;
     end
-    columnDescriptions{i} = cv.description;
-    if ~index && ...
-            (isa(cv,'types.hdmf_common.DynamicTableRegion') ||...
-            isa(cv,'types.core.DynamicTableRegion'))
-        row_idxs = cv.data;
-        ref_table = cv.table.target;
-        cv = cell(length(row_idxs),1);
-        for r = 1:length(row_idxs)
-            cv{r,1} = ref_table.getRow(row_idxs(r)+1);
+    
+    if isempty(DynamicTable.id)
+        matlabTable = table({}, 'VariableNames', [{'id'} DynamicTable.colnames]);
+        return;
+    end
+    
+    % initialize table with id column
+    if isa(DynamicTable.id.data, 'types.untyped.DataStub')...
+            || isa(DynamicTable.id.data, 'types.untyped.DataPipe')
+        ids = DynamicTable.id.data.load();
+    else
+        ids = DynamicTable.id.data;
+    end
+    matlabTable = table( ...
+        ids, ...
+        'VariableNames', {'id'} ...
+    );
+    
+    % deal with DynamicTableRegion columns when index is false
+    [columns, remainingColumns] = deal(DynamicTable.colnames);
+    columnDescriptions = repmat({''}, 1, length(columns));
+    
+    for i = 1:length(columns)
+        cn = columns{i};
+        if isprop(DynamicTable, cn)
+            cv = DynamicTable.(cn);
+        elseif isprop(DynamicTable, 'vectorindex') && DynamicTable.vectorindex.isKey(cn) % Schema version < 2.3.0
+            cv = DynamicTable.vectorindex.get(cn);
+        else
+            cv = DynamicTable.vectordata.get(cn);
         end
-        matlabTable.(cn) = cv;
-        remainingColumns = setdiff(remainingColumns, cn, 'stable');
-    else
-        % pass
+        columnDescriptions{i} = cv.description;
+        if ~index && ...
+                (isa(cv,'types.hdmf_common.DynamicTableRegion') ||...
+                isa(cv,'types.core.DynamicTableRegion'))
+            row_idxs = cv.data;
+            ref_table = cv.table.target;
+            cv = cell(length(row_idxs),1);
+            for r = 1:length(row_idxs)
+                cv{r,1} = ref_table.getRow(row_idxs(r)+1);
+            end
+            matlabTable.(cn) = cv;
+            remainingColumns = setdiff(remainingColumns, cn, 'stable');
+        else
+            % pass
+        end
     end
+    % append remaining columns to table
+    % making the assumption that length of ids reflects table height
+    matlabTable = [matlabTable DynamicTable.getRow( ...
+        1:length(ids), ...
+        'columns', remainingColumns ...
+    )];
+    
+    % Update the columns order to be the same as the original
+    if iscolumn(columns); columns = transpose(columns); end
+    matlabTable = matlabTable(:, [{'id'}, columns]);
+    
+    % Add variable descriptions
+    matlabTable.Properties.VariableDescriptions = [{''}, columnDescriptions];
 end
-% append remaining columns to table
-% making the assumption that length of ids reflects table height
-matlabTable = [matlabTable DynamicTable.getRow( ...
-    1:length(ids), ...
-    'columns', remainingColumns ...
-)];
-
-% Update the columns order to be the same as the original
-if iscolumn(columns); columns = transpose(columns); end
-matlabTable = matlabTable(:, [{'id'}, columns]);
-
-% Add variable descriptions
-matlabTable.Properties.VariableDescriptions = [{''}, columnDescriptions];
