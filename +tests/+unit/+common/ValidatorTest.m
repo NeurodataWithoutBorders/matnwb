72025833354ede56516cd150d68b44515607dd12
ehennestad <ehennestad@gmail.com>
2026-04-14 12:13:37 +0200

    Add compatibility validators for DynamicTable and VectorData

--- +tests/+unit/+common/ValidatorTest.m
+++ +tests/+unit/+common/ValidatorTest.m
@@ -7,5 +7,25 @@ classdef ValidatorTest < matlab.unittest.TestCase
                 @(vn) matnwb.common.mustBeValidSchemaVersion('1.0'), ...
                 'NWB:VersionValidator:InvalidVersionNumber')
         end
+
+        function testDynamicTableValidator(testCase)
+            
+            % Verify that validation passes for DynamicTable object
+            try
+                dynamicTable = types.hdmf_common.DynamicTable();
+                matnwb.common.validation.mustBeDynamicTable(dynamicTable)
+            catch exception
+                diagnosticMessage = sprintf(...
+                    ['Unexpected validation failure for DynamicTable. ', ...
+                    'Reason: %s'], exception.message);
+                testCase.verifyFail(diagnosticMessage)
+            end
+
+            % Verify that validation fails for non-DynamicTable object
+            vectorData = types.hdmf_common.VectorData();
+            testCase.verifyError(...
+                @() matnwb.common.validation.mustBeDynamicTable(vectorData), ...
+                'NWB:validators:mustBeDynamicTable')
+        end
     end 
 end
