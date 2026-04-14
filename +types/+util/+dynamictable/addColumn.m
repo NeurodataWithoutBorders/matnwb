2350f5883c6b3a05eacfa6e2a6899d6383402971
ehennestad <ehennestad@gmail.com>
2026-04-14 22:24:26 +0200

    Add argument blocks, improve and unify variable names

--- +types/+util/+dynamictable/addColumn.m
+++ +types/+util/+dynamictable/addColumn.m
@@ -1,4 +1,4 @@
-function addColumn(DynamicTable, varargin)
+function addColumn(dynamicTable, varargin)
 % ADDCOLUMN Given a dynamic table and a set of keyword arguments for one or
 % more columns, add one or more columns to the dynamic table by providing
 % either keywords or a MATLAB table
@@ -15,24 +15,28 @@ function addColumn(DynamicTable, varargin)
 % 2) The height of the columns to be appended matches the height of the
 % existing columns
 
-    validateattributes(DynamicTable,...
-        {'types.core.DynamicTable', 'types.hdmf_common.DynamicTable'},...
-        {'scalar'});
-    
-    assert(nargin > 1, 'NWB:DynamicTable:AddColumn:NoData', 'Not enough arguments');
-    
-    if isempty(DynamicTable.id)
-        types.util.dynamictable.internal.initDynamicTableId(DynamicTable);
+    arguments
+        dynamicTable (1,1) {matnwb.common.validation.mustBeDynamicTable}
+    end
+    arguments (Repeating)
+        varargin
     end
     
-    assert(~isa(DynamicTable.id.data, 'types.untyped.DataStub'),...
+    assert(nargin > 1, 'NWB:DynamicTable:AddColumn:NoData', ...
+        'Not enough arguments');
+    
+    if isempty(dynamicTable.id)
+        types.util.dynamictable.internal.initDynamicTableId(dynamicTable);
+    end
+    
+    assert(~isa(dynamicTable.id.data, 'types.untyped.DataStub'),...
         'NWB:DynamicTable:AddColumn:Uneditable',...
         ['Cannot write to on-file Dynamic Tables without enabling data pipes. '...
         'If this was produced with pynwb, please enable chunking for this table.']);
     
     if istable(varargin{1})
-        types.util.dynamictable.addTableColumn(DynamicTable, varargin{:});
+        types.util.dynamictable.addTableColumn(dynamicTable, varargin{:});
     else
-        types.util.dynamictable.addVarargColumn(DynamicTable, varargin{:});
+        types.util.dynamictable.addVarargColumn(dynamicTable, varargin{:});
     end
 end
