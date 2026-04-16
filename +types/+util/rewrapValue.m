652d52544dbdbdc54611d6bf519906f8cbad1fb4
ehennestad <ehennestad@gmail.com>
2026-04-16 12:26:29 +0200

    Fix unwrap and rewrap of nested values

--- +types/+util/rewrapValue.m
+++ +types/+util/rewrapValue.m
@@ -1,11 +1,16 @@
 function originalValue = rewrapValue(value, originalValue)
 
-    if isa(originalValue, 'types.untyped.Anon')
-        originalValue.value = value;
-    elseif isa(originalValue, 'types.untyped.DatasetClass') && isprop(originalValue, 'data')
-        originalValue.data = value;
+    originalValue = rewrapNestedValue(value, originalValue);
+end
+
+function wrappedValue = rewrapNestedValue(value, wrappedValue)
+
+    if isa(wrappedValue, 'types.untyped.Anon')
+        wrappedValue.value = rewrapNestedValue(value, wrappedValue.value);
+    elseif isa(wrappedValue, 'types.untyped.DatasetClass') && isprop(wrappedValue, 'data')
+        wrappedValue.data = value;
     else
         error('NWB:RewrapValue:UnsupportedType', ...
-            'Can not unwrap value of type %s. Please report', class(originalValue))
+            'Can not unwrap value of type %s. Please report', class(wrappedValue))
     end
 end
