cddb398be101d3e3efd77030f13105bd26d1de70
ehennestad <ehennestad@gmail.com>
2026-04-21 21:40:03 +0200

    Improve read performance

--- +io/+backend/+zarr2/+mw/ensureAvailable.m
+++ +io/+backend/+zarr2/+mw/ensureAvailable.m
@@ -1,6 +1,12 @@
 function ensureAvailable()
 % ensureAvailable - Validate that the MathWorks Zarr wrapper is on path.
 
+    persistent isValidated
+
+    if isequal(isValidated, true)
+        return
+    end
+
     requiredFunctions = ["zarrinfo", "zarrread", "readZattrs"];
     isMissing = arrayfun(@(name) exist(name, "file") == 0, requiredFunctions);
 
@@ -9,4 +15,6 @@ function ensureAvailable()
             "The MathWorks Zarr wrapper is required on the MATLAB path. Missing function(s): %s", ...
             strjoin(requiredFunctions(isMissing), ", "))
     end
+
+    isValidated = true;
 end
